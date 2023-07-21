b18b81dec28e252fe7d837cfb77b8cee7ccf5aaa
Joep Meindertsma <joep@ontola.io>
2023-07-21 08:51:05 +0200

    redirect

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,10 @@
 export const prerender = true
+import { redirect } from '@sveltejs/kit'
 
 export async function load({ url }) {
+	if (url.host === 'pauseai.org') {
+		return redirect(301, 'https://pauseai.info' + url.pathname)
+	}
 	return {
 		url: url.pathname
 	}
