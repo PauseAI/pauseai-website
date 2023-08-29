4dbe11d08149636fb6a9d2cd686ec60bddabe3d5
Joep Meindertsma <joep@ontola.io>
2023-08-29 17:45:10 +0200

    Improve twitter preview

--- src/lib/components/PostMeta.svelte
+++ src/lib/components/PostMeta.svelte
@@ -11,6 +11,12 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="article:published_time" content={date} />
 	<meta property="og:image" content={image} />
+	<meta property="article:published_time" content={date} />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:card" content="summary" />
+	<meta property="twitter:image" content={image} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:site" content="@PauseAI" />
+	<meta property="twitter:creator" content="@PauseAI" />
 </svelte:head>
