7f10ac319b78298d73f25269446c12333c44160a
Joep Meindertsma <joep@ontola.io>
2023-12-14 21:38:33 +0100

    email builder wip

---
+++ src/routes/email-builder/+page.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import PostMeta from '$lib/components/PostMeta.svelte'
+	import type { Post } from '$lib/types'
+	import { countries, type Country } from './countries'
+	import Button from '$lib/components/Button.svelte'
+
+	let top: HTMLHeadingElement
+	let { title, description, date }: Post = {
+		title: 'Email Builder',
+		slug: 'email-builder',
+		description: 'A web app to help you write an email to a politician. Convince them to Pause AI!',
+		date: '2023-12-08',
+		categories: []
+	}
+
+	const letterId = 'letter'
+	let selectedCountry: Country = countries[0]
+
+	function copyHTMLWithoutStyles() {
+		var element = document.getElementById(letterId)
+		var clonedElement = element?.cloneNode(true)
+		navigator.clipboard
+			.write([
+				new ClipboardItem({
+					'text/html': new Blob([clonedElement?.outerHTML], { type: 'text/html' })
+				})
+			])
+			.then(() => {
+				window.alert('Letter copied to clipboard!')
+			})
+			.catch((err) => {
+				window.alert(`Failed to copy: ${err}`)
+			})
+	}
+</script>
+
+<PostMeta {title} {description} {date} />
+
+<div class="header">
+	<h1>{title}</h1>
+	<em>{description}</em>
+</div>
+
+<h2>Why sending an email is awesome</h2>
+<ul>
+	<li>
+		<b>Exit your filter bubble.</b> If you're talking about AI risks or pausing in a discord server or
+		twitter, you're mostly preaching to the choir. With email, you can reach people who don't read about
+		this stuff all day.
+	</li>
+	<li>
+		<b>It's the medium for the pros.</b> Politicians, journalists, lobbyists - all of them use email.
+		If you want to be taken seriously, you should use email too.
+	</li>
+	<li>
+		<b>No social pressure.</b> If you post something publicly, a politician might be hesitant to respond
+		to a topic they haven't made up their mind on.
+	</li>
+</ul>
+
+<h2>Who to send to</h2>
+<ul>
+	<li>awdaw</li>
+	<li />
+</ul>
+
+<div class="actionBar">
+	<select bind:value={selectedCountry}>
+		{#each countries as country}
+			<option value={country}>{country.name}</option>
+		{/each}
+	</select>
+	<Button on:click={() => copyHTMLWithoutStyles()}>Copy</Button>
+</div>
+
+<div class="letter" id={letterId} contenteditable="true">
+	<svelte:component this={selectedCountry.mail} />
+</div>
+
+<h2>Tips for adjusting the letter</h2>
+<ul>
+	<li>
+		<b>Know your audience.</b> Read up about the person you're sending a letter to. What are they working
+		on? How do they think about AI? What has happened in their professional life the last weeks?
+	</li>
+	<li>
+		<b>Share something about yourself.</b> Why you care about AI safety? Why did you take the time to
+		send this letter?
+	</li>
+	<li>
+		<b>Make it newsworthy.</b> The mail template is not always up-to-date. Make sure you mention recent
+		AI policy advancements (especially local ones).
+	</li>
+	<li>
+		<b>Have a clear ask.</b> What do you want the reader to do? Make this something concrete, something
+		achieveable. Maybe a face to face meeting to discuss the issue. Maybe ask for a debate to be organized
+		in parliament.
+	</li>
+</ul>
+
+<style>
+	ul {
+		list-style: disc;
+		margin-left: 2rem;
+	}
+
+	.letter {
+		/* Edit mouse cursor, indicate editable */
+		cursor: text;
+		/* make it lookt like a letter! */
+		background-color: #fff;
+		padding: 1rem;
+		margin: 1rem;
+		/* shadow */
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+		font-family: 'Times New Roman', Times, serif;
+	}
+
+	.actionBar {
+		display: flex;
+		align-items: center;
+		margin-bottom: 1rem;
+		gap: 1rem;
+		justify-content: end;
+	}
+
+	select {
+		padding: 0.6rem;
+		border-radius: 10px;
+	}
+</style>
---
+++ src/routes/email-builder/Dutch.svelte
@@ -0,0 +1,86 @@
+<p>Dear __PERSON__,</p>
+<p>
+	First of all, thank you very much for everything you have done for __THING__. I am emailing you
+	today to bring an issue to your attention, in which I believe the Netherlands and you in
+	particular can play a very important role. The issue is the existential threat of artificial
+	intelligence.
+</p>
+<p>
+	Half of AI researchers <a href="https://aiimpacts.org/2022-expert-survey-on-progress-in-ai/"
+		>believe</a
+	>
+	that there is a 10% or greater chance that the invention of artificial superintelligence will mean
+	the end of humanity. Among AI safety scientists, this chance is
+	<a
+		href="https://forum.effectivealtruism.org/posts/8CM9vZ2nnQsWJNsHx/existential-risk-from-ai-survey-results"
+		>estimated</a
+	>
+	to be an average of 30%. Notable examples of individuals sounding the alarm are Prof.
+	<a href="https://www.youtube.com/watch?v=Y6Sgp7y178k">Geoffrey Hinton</a>
+	and Prof.
+	<a href="https://yoshuabengio.org/2023/05/22/how-rogue-ais-may-arise/">Yoshua Bengio</a>
+	, both Turing-award winners and pioneers of the deep learning methods that are currently achieving
+	the most success. The existential risk of AI has been acknowledged by
+	<a href="https://www.safe.ai/statement-on-ai-risk">hundreds of scientists</a>, the
+	<a href="https://twitter.com/gcolbourn/status/1669104216307015680">UN</a>, the US and recently the
+	<a href="https://twitter.com/EU_Commission/status/1702295053668946148">EU</a>. The latest report
+	on “Generatieve AI” by Rathenau also highlights these risks.
+</p>
+
+<p>
+	To make a long story short: we don't know how to align AI with the complex goals and values that
+	humans have. When a superintelligent system is realised, there is a significant risk that one of
+	these instances will pursue a misaligned goal without us being able to stop it. And even if such a
+	superhuman AI will remain under human control, the one wielding such a power could use this to
+	drastically, irreversibly change the world. Such an AI could be used to develop new technologies
+	and weapons, manipulate masses of people or topple governments.
+</p>
+
+<p>
+	The advancements in the AI landscape have progressed much faster than anticipated. In 2020, it was
+	<a href="https://www.metaculus.com/questions/3479/date-weakly-general-ai-is-publicly-known"
+		>estimated</a
+	>
+	that an AI would pass university entrance exams by 2050. This goal was achieved in March 2023 by the
+	system GPT-4 from OpenAI. These massive, unexpected leaps have prompted many experts to request a pause
+	in AI development through an open letter to major AI companies. The
+	<a href="https://futureoflife.org/open-letter/pause-giant-ai-experiments/">letter</a>
+	has been signed over 33,000 times so far, including many AI researchers and tech figures.
+</p>
+
+<p>
+	Unfortunately, it seems that companies are not willing to jeopardise their competitive position by
+	voluntarily halting development. A pause would need to be imposed by a government. Luckily, there
+	seems to be broad support for slowing down AI development. A recent
+	<a
+		href="https://www.vox.com/future-perfect/2023/9/19/23879648/americans-artificial-general-intelligence-ai-policy-poll"
+		>poll</a
+	>
+	indicates that 63% of American support regulations to prevent AI companies from building superintelligent
+	AI. At the national level, a pause is also challenging because countries have incentives to not fall
+	behind in AI capabilities. That's why we need an international solution.
+</p>
+
+<p>
+	The UK organised an AI Safety Summit on November 1st and 2nd at Bletchley Park. We hoped that
+	during this summit, leaders will work towards sensible solutions that prevent the very worst of
+	the risks that AI poses. The Summit did not lead to any international agreement or policy. We have
+	seen proposals being written by the
+	<a href="https://twitter.com/SenBlumenthal/status/1700147410880569475">US Senate</a>, and even
+	among AI company CEOs there is
+	<a
+		href="https://www.pbs.org/newshour/politics/watch-overwhelming-consensus-for-artificial-intelligence-regulation-musk-says-after-senate-tech-meeting"
+		>“overwhelming consensus”</a
+	>
+	that regulation is needed. Unfortunately,
+	<a href="https://twitter.com/DanielColson6/status/1704976418596352342">none</a>
+	of the existing proposals would do anything to slow down or prevent a superintelligent AI from being
+	created. I am afraid that lobbying efforts by AI companies to keep regulation at a minimum are turning
+	out to be highly effective.
+</p>
+
+<p>__ACTION__</p>
+
+<p>Best regards,</p>
+
+<p>__YOUR NAME__</p>
