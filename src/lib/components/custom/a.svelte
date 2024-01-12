aee90539d92b8fe0af58bb37d5344683957dd8a4
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-01-12 01:04:04 +0100

    Improve external links in text and footer

--- src/lib/components/custom/a.svelte
+++ src/lib/components/custom/a.svelte
@@ -3,18 +3,27 @@
 	import { ExternalLink } from 'lucide-svelte'
 
 	export let href: string
+	export let target: string | null = null
+
+	const RELATIVE_ICON_SIZE = 0.8
+
 	let isExternal = false
+	let anchor: HTMLAnchorElement
+	let iconSize: number;
 
 	onMount(() => {
+		iconSize = parseFloat(getComputedStyle(anchor).fontSize) * RELATIVE_ICON_SIZE
 		isExternal = href.startsWith('http') || href.startsWith('https')
 	})
 </script>
 
-<a {href}>
+<a bind:this={anchor} {href} {target}>
 	<slot />{#if isExternal}
-		<div class="icon">
-			<ExternalLink />
-		</div>
+		<span style='white-space: nowrap'>
+			<div class="icon">
+				<ExternalLink size={iconSize} />
+			</div>
+		</span>
 	{/if}
 </a>
 
@@ -22,6 +31,6 @@
 	.icon {
 		display: inline-block;
 		vertical-align: middle;
-		transform: scale(0.8);
+		margin-left: 0.1em;
 	}
 </style>
