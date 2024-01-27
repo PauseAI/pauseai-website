273ad7023fb50a88d4f8b2a2065efc3811ce004c
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-01-27 02:34:29 +0100

    Email builder improvements

--- src/routes/email-builder/+page.svelte
+++ src/routes/email-builder/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import PostMeta from '$lib/components/PostMeta.svelte'
-	import type { Post } from '$lib/types'
 	import Button from '$lib/components/Button.svelte'
 	// Risks
 	import Xrisk from './concerns/xrisk.svelte'
@@ -139,7 +138,6 @@
 		to someone who's already worked on IT and cybersecurity issues before, consider focsing on that particular
 		issue.
 	</li>
-	<li></li>
 	<li>
 		<b>Select one:</b>
 		{#each concerns as section}
@@ -155,7 +153,7 @@
 <ul>
 	<li>
 		What do you want the recipient to do after receiving your mail? Prepare for the summit, organize
-		a debate, have a meeting?
+		a debate, have a meeting? As with every section, you can replace the suggested text if you have a better idea.
 	</li>
 	<li>
 		<b>Select one:</b>
@@ -168,15 +166,35 @@
 	</li>
 </ul>
 
-<div class="actionBar">
-	<Button on:click={() => copyHTMLWithoutStyles()}>Copy</Button>
-</div>
+<h2>Last steps</h2>
+<p>Before sending the email you have to manually replace "__THING__" and "__COUNTRY__".
+	It can also be effective to further personalise the message. Here are some tips:
+</p>
+<ul>
+	<li>
+		<b>Know your audience.</b> Read up about the person you're sending a letter to. What are they working
+		on? How do they think about AI? What has happened in their professional life the last weeks?
+	</li>
+	<li>
+		<b>Share something about yourself.</b> Why you care about AI safety? Why did you take the time to
+		send this letter?
+	</li>
+	<li>
+		<b>Make it newsworthy.</b> The mail template is not always up-to-date. Make sure you mention recent
+		AI policy advancements (especially local ones).
+	</li>
+</ul>
+<p>
+	For more information, you can take a look at our page on <a href="/writing-a-letter">how to write a letter or email to someone in power</a>.
+</p>
 
+<h2>Result</h2>
+<p>You can edit the message directly in the browser.</p>
 <div class="letter" id={letterId} contenteditable="true">
 	<p>Dear {name || '__ENTER_NAME__'},</p>
 	<p>
 		First of all, thank you very much for everything you have done for __THING__. I am emailing you
-		today to bring an issue to your attention, in which I believe the Netherlands and you in
+		today to bring an issue to your attention, in which I believe __COUNTRY__ and you in
 		particular can play a very important role. The issue is the existential threat of artificial
 		intelligence.
 	</p>
@@ -215,7 +233,7 @@
 		the risks that AI poses. The Summit did not lead to any international agreement or policy. We
 		have seen proposals being written by the
 		<a href="https://twitter.com/SenBlumenthal/status/1700147410880569475">US Senate</a>, and even
-		among AI company CEOs there is
+		among AI company CEOs, there is
 		<a
 			href="https://www.pbs.org/newshour/politics/watch-overwhelming-consensus-for-artificial-intelligence-regulation-musk-says-after-senate-tech-meeting"
 			>“overwhelming consensus”</a
@@ -234,26 +252,9 @@
 	<p>__YOUR NAME__</p>
 </div>
 
-<h2>Tips for adjusting the letter</h2>
-<ul>
-	<li>
-		<b>Know your audience.</b> Read up about the person you're sending a letter to. What are they working
-		on? How do they think about AI? What has happened in their professional life the last weeks?
-	</li>
-	<li>
-		<b>Share something about yourself.</b> Why you care about AI safety? Why did you take the time to
-		send this letter?
-	</li>
-	<li>
-		<b>Make it newsworthy.</b> The mail template is not always up-to-date. Make sure you mention recent
-		AI policy advancements (especially local ones).
-	</li>
-	<li>
-		<b>Have a clear ask.</b> What do you want the reader to do? Make this something concrete, something
-		achievable. Maybe a face to face meeting to discuss the issue. Maybe ask for a debate to be organized
-		in parliament.
-	</li>
-</ul>
+<div class="actionBar">
+	<Button on:click={() => copyHTMLWithoutStyles()}>Copy</Button>
+</div>
 
 <style>
 	ul {
