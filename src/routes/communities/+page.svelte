fbdc9834532ba156ba370e8462ab540857420f84
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-02-07 08:49:28 +0100

    Limit zoom for community map geolocation button

--- src/routes/communities/+page.svelte
+++ src/routes/communities/+page.svelte
@@ -40,13 +40,14 @@
 
 		map.addControl(
 			new GeolocateControl({
+				fitBoundsOptions: {
+					maxZoom: 4
+				},
 				positionOptions: {
 					enableHighAccuracy: true
 				},
 				// When active the map will receive updates to the device's location as it changes.
 				trackUserLocation: true,
-				// Draw an arrow next to the location dot to indicate which direction the device is heading.
-				showUserHeading: true
 			})
 		)
 
