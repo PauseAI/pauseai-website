7978962bf4d8686f56de2e3ebe6ecc54de23bcfb
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-02-11 17:20:12 +0100

    Configure pagefind as external dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import type { UserConfig } from 'vite'
 
-const config = {
+const config: UserConfig = {
+	build: {
+		rollupOptions: {
+			external: ['/_pagefind/pagefind.js']
+		}
+	},
 	server: {
 		port: 37572
 	},
