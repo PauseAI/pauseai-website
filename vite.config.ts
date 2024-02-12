b22941a466baf574151cecadbc6860c3aa1058ae
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-02-12 12:50:24 +0100

    vite options

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import type { UserConfig } from 'vite'
 
-const config = {
+const config: UserConfig = {
 	server: {
 		port: 37572
 	},
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['/pagefind/pagefind.js']
+		}
+	},
+	assetsInclude: '**/pagefind.js'
 }
 
 export default config
