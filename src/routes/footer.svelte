d36cc418d2af710dbc49d4a3b7848e28f1053834
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-02-14 11:32:10 +0100

    Add full text search using Pagefind

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -4,56 +4,58 @@
 	import Edit from '$lib/components/Edit.svelte'
 </script>
 
-<a class="c2a" href="/join">Join PauseAI ></a>
-<div class="bar" />
 <footer>
-	<div class="column">
-		<h2>Info</h2>
-		<a href="/faq">FAQ</a>
-		<a href="/proposal">Proposal</a>
-		<a href="/learn">Learn</a>
-		<a href="/sota">State of AI</a>
-		<a href="/psychology-of-x-risk">Psychology of x-risk</a>
-		<a href="/4-levels-of-ai-regulation">4 Levels of regulation</a>
-		<ExternalLink href="https://pauseai-shop.fourthwall.com" target="_blank">Merchandise</ExternalLink>
-	</div>
-	<div class="column">
-		<h2>Risks</h2>
-		<a href="/outcomes">AI Outcomes</a>
-		<a href="/risks">Risks overview</a>
-		<a href="/xrisk">Existential risk</a>
-		<a href="/ai-takeover">AI takeover</a>
-		<a href="/cybersecurity-risks">Cybersecurity</a>
-		<a href="/urgency">Urgency</a>
-		<a href="/dangerous-capabilities">Capabilities</a>
-	</div>
-	<div class="column">
-		<h2>Take Action</h2>
-		<a href="/join">Join PauseAI</a>
-		<a href="/communities">Local communities</a>
-		<a href="/action">How you can help</a>
-		<a href="/protests">Protests</a>
-		<a href="/organizing-a-protest">Organize a protest</a>
-		<a href="/writing-a-letter">Write a letter</a>
-		<a href="/lobby-tips">Lobby tips</a>
-	</div>
-	<div class="column">
-		<h2>Socials</h2>
-		<ExternalLink href="https://discord.gg/2XXWXvErfA" target="_blank">Discord</ExternalLink>
-		<ExternalLink href="https://twitter.com/PauseAI" target="_blank">Twitter</ExternalLink>
-		<ExternalLink href="https://www.linkedin.com/company/pauseai" target="_blank">LinkedIn</ExternalLink>
-		<ExternalLink href="https://www.facebook.com/PauseAI/" target="_blank">Facebook</ExternalLink>
-		<ExternalLink href="https://www.tiktok.com/@pauseai" target="_blank">TikTok</ExternalLink>
-		<ExternalLink href="https://www.instagram.com/pause_ai/" target="_blank">Instagram</ExternalLink>
-		<ExternalLink href="https://www.youtube.com/@PauseAI" target="_blank">YouTube</ExternalLink>
-	</div>
-	<div class="column">
-		<h2>Other</h2>
-		<Toggle />
-		<Edit />
-		<a href="/posts">All pages</a>
-		<a href="/rss.xml" target="_blank">RSS</a>
-		<ExternalLink href="https://creativecommons.org/licenses/by/4.0/" target="_blank">License: CC-BY 4.0</ExternalLink>
+	<a class="c2a" href="/join">Join PauseAI ></a>
+	<div class="bar" />
+	<div class="footer-links">
+		<div class="column">
+			<h2>Info</h2>
+			<a href="/faq">FAQ</a>
+			<a href="/proposal">Proposal</a>
+			<a href="/learn">Learn</a>
+			<a href="/sota">State of AI</a>
+			<a href="/psychology-of-x-risk">Psychology of x-risk</a>
+			<a href="/4-levels-of-ai-regulation">4 Levels of regulation</a>
+			<ExternalLink href="https://pauseai-shop.fourthwall.com" target="_blank">Merchandise</ExternalLink>
+		</div>
+		<div class="column">
+			<h2>Risks</h2>
+			<a href="/outcomes">AI Outcomes</a>
+			<a href="/risks">Risks overview</a>
+			<a href="/xrisk">Existential risk</a>
+			<a href="/ai-takeover">AI takeover</a>
+			<a href="/cybersecurity-risks">Cybersecurity</a>
+			<a href="/urgency">Urgency</a>
+			<a href="/dangerous-capabilities">Capabilities</a>
+		</div>
+		<div class="column">
+			<h2>Take Action</h2>
+			<a href="/join">Join PauseAI</a>
+			<a href="/communities">Local communities</a>
+			<a href="/action">How you can help</a>
+			<a href="/protests">Protests</a>
+			<a href="/organizing-a-protest">Organize a protest</a>
+			<a href="/writing-a-letter">Write a letter</a>
+			<a href="/lobby-tips">Lobby tips</a>
+		</div>
+		<div class="column">
+			<h2>Socials</h2>
+			<ExternalLink href="https://discord.gg/2XXWXvErfA" target="_blank">Discord</ExternalLink>
+			<ExternalLink href="https://twitter.com/PauseAI" target="_blank">Twitter</ExternalLink>
+			<ExternalLink href="https://www.linkedin.com/company/pauseai" target="_blank">LinkedIn</ExternalLink>
+			<ExternalLink href="https://www.facebook.com/PauseAI/" target="_blank">Facebook</ExternalLink>
+			<ExternalLink href="https://www.tiktok.com/@pauseai" target="_blank">TikTok</ExternalLink>
+			<ExternalLink href="https://www.instagram.com/pause_ai/" target="_blank">Instagram</ExternalLink>
+			<ExternalLink href="https://www.youtube.com/@PauseAI" target="_blank">YouTube</ExternalLink>
+		</div>
+		<div class="column">
+			<h2>Other</h2>
+			<Toggle />
+			<Edit />
+			<a href="/posts">All pages</a>
+			<a href="/rss.xml" target="_blank">RSS</a>
+			<ExternalLink href="https://creativecommons.org/licenses/by/4.0/" target="_blank">License: CC-BY 4.0</ExternalLink>
+		</div>
 	</div>
 </footer>
 
@@ -66,7 +68,7 @@
 		margin-top: 0.5rem;
 	}
 
-	footer {
+	.footer-links {
 		display: grid;
 		grid-template-columns: repeat(5, 1fr);
 		gap: 1rem;
@@ -89,7 +91,7 @@
 		flex: 1;
 	}
 
-	footer h2 {
+	.footer-links h2 {
 		font-size: 0.9rem;
 		font-family: var(--font-heading);
 		font-weight: 700;
@@ -107,12 +109,12 @@
 		text-align: left;
 	}
 
-	footer :global(a) {
+	.footer-links :global(a) {
 		color: var(--text-2);
 		text-decoration: none;
 	}
 
-	footer :global(a:hover) {
+	.footer-links :global(a:hover) {
 		text-decoration: underline;
 	}
 </style>
