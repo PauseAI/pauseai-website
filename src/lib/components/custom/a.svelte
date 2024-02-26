640807b438fc026648beb2539f21dd5922cc60a8
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-02-26 12:54:48 +0100

    Add smooth scrolling to section links

--- src/lib/components/custom/a.svelte
+++ src/lib/components/custom/a.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
 	import ExternalLink from 'lucide-svelte/icons/external-link'
+	import { page } from '$app/stores'
+	import { pushState } from '$app/navigation'
 
 	export let href: string
 	export let target: string | null = null
 
 	let isExternal = false
+	let anchor: HTMLAnchorElement
 
 	onMount(() => {
 		isExternal = href.startsWith('http:') || href.startsWith('https:') || href.startsWith('mailto:')
+		if (href.startsWith('#')) {
+			anchor.addEventListener('click', (ev) => {
+				ev.preventDefault()
+				const url = $page.url
+				url.hash = href
+				pushState(url, $page.state)
+				const target = document.querySelector(href) as HTMLElement | null
+				if (!target) return
+				target.scrollIntoView({ behavior: 'smooth' })
+				target.tabIndex = -1
+				target.focus({ preventScroll: true })
+			})
+		}
 	})
 </script>
 
-<a {href} {target}>
+<a {href} {target} bind:this={anchor}>
 	<slot />{#if isExternal}
 		<span style="white-space: nowrap">
 			<div class="icon">
