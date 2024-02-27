5e9895de57ce407d763edcd4bcc288ebb289c579
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-02-27 13:39:04 +0100

    Make TOC head sticky

--- src/lib/components/Toc.svelte
+++ src/lib/components/Toc.svelte
@@ -4,15 +4,9 @@
 	import X from 'lucide-svelte/icons/x'
 	import List from 'lucide-svelte/icons/list'
 
-	let closeButton: HTMLElement
 	let desktop: boolean | undefined
 	let open: boolean | undefined
-	let nav: HTMLElement | undefined
 	let headings: HTMLHeadingElement[] | undefined
-
-	$: {
-		nav?.appendChild(closeButton)
-	}
 </script>
 
 {#if open}
@@ -23,25 +17,26 @@
 		headingSelector=":is(h2, h3, h4):not(.toc-exclude):not(footer *)"
 		title="Contents"
 		bind:open
-		bind:nav
 		bind:desktop
 		bind:headings
 		hide={(headings?.length ?? 0) <= 1}
 	>
 		<svelte:fragment slot="open-toc-icon">
-			<List size="2rem"/>
+			<List size="2rem" />
+		</svelte:fragment>
+		<svelte:fragment slot="title">
+			<div class="toc-head">
+				<h2 class="toc-title-heading">Contents</h2>
+				<button
+					class="toc-close"
+					on:click={() => (open = false)}
+				>
+					<X size="1.2rem" />
+				</button>
+			</div>
 		</svelte:fragment>
 	</Toc>
 </div>
-
-<button
-	id="toc-close"
-	on:click={() => (open = false)}
-	bind:this={closeButton}
-	style={nav ? '' : 'display: none;'}
->
-	<X size="1.2rem" />
-</button>
 
 <style>
 	:root {
@@ -51,7 +46,7 @@
 		--toc-mobile-bg: var(--bg);
 		--toc-active-bg: var(--brand);
 		--toc-max-height: 80vh;
-		--toc-padding: 1em;
+		--toc-padding: 0em 1em 1em;
 		--toc-z-index: 10;
 	}
 
@@ -65,11 +60,23 @@
 		left: 0;
 	}
 
+	.toc-head {
+		position: sticky;
+		top: 0;
+		padding: 1em 0em 0.5em;
+		background-color: var(--bg);
+	}
+
+	.toc-title-heading {
+		padding: 0;
+		margin: 0;
+	}
+
 	.toc-wrapper :global(aside.toc > nav) {
 		max-width: 90vw;
 	}
 
-	#toc-close {
+	.toc-close {
 		background-color: transparent;
 		border: none;
 		color: var(--text);
@@ -89,7 +96,7 @@
 		.toc-wrapper :global(aside.toc > nav > ol > li:hover) {
 			text-decoration-line: underline;
 		}
-		#toc-close:hover {
+		.toc-close:hover {
 			color: var(--brand);
 		}
 	}
