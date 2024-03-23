1528c95556534cbb6b7e1138e3e15ecdba5aeaed
mrbreastly <61096713+mrbreastly@users.noreply.github.com>
2024-03-22 22:41:36 -0500

    Update and rename meta.ts to meta-expectation.ts

---
+++ src/routes/quotes/meta-expectation.ts
@@ -0,0 +1,9 @@
+import type { Post } from "$lib/types";
+
+export const meta-expectation: Post = {
+  title: 'Expectation:',
+  description: 'Quotes about expectations of the risks from artificial intelligence',
+  date: '2024-03-22',
+  slug: 'quotes-expectation',
+  categories: []
+}
--- src/routes/quotes/meta.ts
+++
@@ -1,9 +0,0 @@
-import type { Post } from "$lib/types";
-
-export const meta: Post = {
-  title: 'Quotes',
-  description: 'Quotes about risks from artificial intelligence',
-  date: '2024-01-26',
-  slug: 'quotes',
-  categories: []
-}
