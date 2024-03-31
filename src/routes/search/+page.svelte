615d417cb9a4a951fd5979e5200cc42a4a0286b7
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-03-31 01:49:30 +0100

    Outsource search to component

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,31 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	// @ts-expect-error
-	import { PagefindUI } from '@pagefind/default-ui'
-	import '@pagefind/default-ui/css/ui.css'
-
-	type Result = {
-		url: string
-		sub_results: {
-			url: string
-		}[]
-	}
-
-	onMount(() => {
-		new PagefindUI({
-			element: '#search',
-			showSubResults: true,
-			showImages: false,
-			processResult: function (result: Result) {
-				result.url = result.url.replace(/(.*)\.html/, '$1')
-				for (const subResult of result.sub_results) {
-					subResult.url = subResult.url.replace(/(.*).html(#.*)?/, '$1$2')
-				}
-			}
-		})
-		const input = document.getElementsByClassName('pagefind-ui__search-input')[0] as HTMLElement
-		input.focus()
-	})
+	import Search from '$lib/components/Search.svelte'
 </script>
 
 <svelte:head>
@@ -35,20 +9,5 @@
 <div data-pagefind-ignore>
 	<h1>Search</h1>
 
-	<div id="search" />
+	<Search />
 </div>
-
-<style>
-	:root {
-		--pagefind-ui-font: inherit;
-		--pagefind-ui-text: var(--text);
-		--pagefind-ui-primary: var(--text);
-		--pagefind-ui-background: var(--bg-subtle);
-		--pagefind-ui-border: var(--brand);
-	}
-
-	#search :global(mark) {
-		background-color: var(--bg-subtle);
-		color: var(--text);
-	}
-</style>
