1bebe88cf59be9065978600720c2bae0af4355bb
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-04-11 16:48:51 +0200

    Exclude /people from prerendering

--- src/routes/people/+page.ts
+++ src/routes/people/+page.ts
@@ -1,5 +1,7 @@
 import type { Person } from '$lib/types'
 
+export const prerender = false
+
 export const load = async ({ fetch }) => {
 	const response = await fetch('api/people')
 	const posts: Person[] = await response.json()
