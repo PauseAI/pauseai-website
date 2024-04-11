0a4d979805c333e9776eecda594c1de0ced3853f
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-04-11 23:53:56 +0200

    Show redits on the bottom of quotes page

---
+++ src/routes/quotes/quote.d.ts
@@ -0,0 +1,9 @@
+export default interface Quote {
+    background: string
+    color: string
+    text: string
+    author: string
+    author_description: string
+    padding?: string
+    notice?: string
+}
