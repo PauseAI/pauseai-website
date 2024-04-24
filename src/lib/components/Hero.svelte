81c8f09d0e57f0de61ef960e76ede395d4567cd4
Joep Meindertsma <joep@ontola.io>
2024-04-24 13:31:08 +0200

    WIP new home page

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,53 @@
+<script type="ts">
+	import london from './../../assets/protests/london.jpeg'
+	import denHaag from './../../assets/protests/den-haag.jpeg'
+	import sf from './../../assets/protests/san-francisco.jpeg'
+</script>
+
+<head> test </head>
+
+<div class="hero">
+	<div class="overlay"></div>
+	<img src={london} alt="" />
+	<img src={denHaag} alt="" />
+	<img src={sf} alt="" />
+	<h2>DON’T LET AI COMPANIES <br />GAMBLE WITH OUR FUTURE</h2>
+</div>
+
+<style>
+	h2 {
+		background-color: black;
+		padding: 2rem;
+		position: absolute;
+		text-transform: uppercase;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: white;
+		font-size: 1rem;
+	}
+	.hero {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-gap: 1rem;
+		height: 400px;
+		overflow: hidden;
+		position: relative;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: var(--brand);
+		opacity: 0.8;
+	}
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+</style>
