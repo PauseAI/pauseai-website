9dc5da63b44cc4879cae9ceaf6d4c7ad0c8d2435
Moiri Gamboni <moiri.gamboni@proton.me>
2024-05-18 17:41:21 +0200

    fix: remove unecessary button element in link buttons

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,10 +4,8 @@
 </script>
 
 {#if href}
-	<a class="button-link" {href}>
-		<button on:click class:subtle>
-			<slot />
-		</button>
+	<a {href} class:subtle>
+		<slot />
 	</a>
 {:else}
 	<button on:click class:subtle>
@@ -16,7 +14,8 @@
 {/if}
 
 <style>
-	button {
+	button,
+	a {
 		background-color: var(--button-default);
 		color: var(--button-text);
 		border: none;
@@ -25,36 +24,40 @@
 		/* font-size: var(--font-size); */
 		font-family: var(--font-body);
 		cursor: pointer;
-		display: flex;
+		display: inline-flex;
 		font-weight: bold;
 		position: relative;
 		min-width: 10rem;
 		height: 3rem;
-		display: flex;
 		align-items: center;
 		justify-content: center;
 		font-size: 1.125rem;
-	}
-	.button-link {
-		/* color: var(--text); */
 		text-decoration: none;
 	}
-	button.subtle {
+	button.subtle,
+	a.subtle {
 		/* background-color: var(--background); */
 		background-color: var(--button-alt-default);
+		color: var(--button-text);
 		/* color: var(--brand); */
 	}
-	button.subtle:hover {
+	button.subtle:hover,
+	a.subtle:hover {
 		/* background-color: var(--background); */
 		background-color: var(--button-alt-hover);
+		color: var(--button-text);
 		/* text-decoration: underline; */
 	}
-	button:hover {
+	button:hover,
+	a:hover {
 		/* background-color: var(--brand-subtle); */
 		background-color: var(--button-hover);
+		color: var(--button-text);
 	}
-	button:active {
+	button:active,
+	a:active {
 		/* background-color: var(--brand); */
 		background-color: var(--button-default);
+		color: var(--button-text);
 	}
 </style>
