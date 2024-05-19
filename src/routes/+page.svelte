7a2eac734d87dba6e13ae8f34930340b9a4cb237
Moiri Gamboni <moiri.gamboni@proton.me>
2024-05-19 16:51:59 +0200

    feat: remove old risks section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Block from '$lib/components/Block.svelte'
 	import Button from '$lib/components/Button.svelte'
 	import Mark from '$lib/components/Mark.svelte'
 	import PostMeta from '$lib/components/PostMeta.svelte'
@@ -34,33 +33,6 @@
 	/>
 	<LeftCorner />
 	<RightCorner />
-</section>
-
-<section>
-	<Block linkText="Read about the risks" href="/risks">
-		<span slot="title">We risk <Mark>losing control</Mark></span>
-		AI can have amazing benefits, but it could also erode our democracy, destabilize our economy and
-		could be used to create powerful cyber weapons.
-	</Block>
-	<Block linkText="Why and how AI can kill us" href="/xrisk">
-		<span slot="title">We risk <Mark>human extinction</Mark></span>
-		Many AI labs and experts agree: AI could end humanity.
-	</Block>
-	<Block linkText="Read the proposal" href="/proposal">
-		<span slot="title">We need a <Mark>pause</Mark></span>
-		Stop the development of AI systems more powerful than GPT-4 until we know how to make them safe.
-		This needs to happen on an international level, and it needs to happen soon.
-	</Block>
-	<Block linkText="How long do we have?" href="/urgency">
-		<span slot="title">WE NEED TO ACT <Mark>RIGHT NOW</Mark></span>
-		In 2020, experts thought we had more than 35 years until AGI. Recent breakthroughs show we might
-		be almost there. Superintelligence could be one innovation away, so we should tread carefully.</Block
-	>
-	<Block linkText="Take action" href="/action">
-		<span slot="title"><Mark>YOU</Mark> CAN HELP</span>
-		Too few people are well-informed about the potential risks of AI. Inform others, and help stop this
-		race to the bottom.</Block
-	>
 </section>
 
 <section class="faq">
