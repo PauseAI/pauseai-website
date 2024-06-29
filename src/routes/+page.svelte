6c8da981f10242f94d04ef20cac2523120bd8968
Moiri Gamboni <moiri.gamboni@proton.me>
2024-06-29 15:17:31 +0200

    chore: move homepage components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import PostMeta from '$lib/components/PostMeta.svelte'
-	import Hero from '../home/hero.svelte'
-	import Lead from '../home/lead.svelte'
-	import Risks from '../home/risks.svelte'
-	import Articles from '../home/articles.svelte'
-	import Faq from '../home/faq.svelte'
+	import Hero from '$lib/components/home/hero.svelte'
+	import Lead from '$lib/components/home/lead.svelte'
+	import Risks from '$lib/components/home/risks.svelte'
+	import Articles from '$lib/components/home/articles.svelte'
+	import Faq from '$lib/components/home/faq.svelte'
 
 	const title = 'We need to Pause AI'
 	const description = 'We are risking human extinction. We need to pause AI development, right now.'
