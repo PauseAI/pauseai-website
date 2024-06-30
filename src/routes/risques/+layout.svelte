062991ab6f14f6b5a5c663e7eb45e44f2d8cb56c
Moiri Gamboni <moiri.gamboni@proton.me>
2024-06-30 18:38:50 +0200

    feat: make nav (dis)appear on scroll

--- src/routes/risques/+layout.svelte
+++ src/routes/risques/+layout.svelte
@@ -1,20 +1,54 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+	import { slide } from 'svelte/transition'
 	import MoveLeftIcon from '$lib/components/icons/move-left.svelte'
 	import MoveRightIcon from '$lib/components/icons/move-right.svelte'
 	export let data
 	const index = data.posts.findIndex((post) => post.slug === data.url)
 	const prev = data.posts[index - 1]
 	const next = data.posts[index + 1]
+
+	let y = 0
+	let lastY = 0
+	let isVisible = true
+	let navHeight: number
+	let ticking = false
+
+	function handleScroll() {
+		if (!ticking) {
+			window.requestAnimationFrame(() => {
+				const scrollThreshold = navHeight || 130
+				isVisible = y < lastY || y < scrollThreshold
+				lastY = y
+				ticking = false
+			})
+			ticking = true
+		}
+	}
+
+	onMount(() => {
+		const nav = document.querySelector('nav')
+		if (nav) {
+			navHeight = nav.offsetHeight
+		}
+	})
 </script>
 
-<nav>
-	{#if prev}
-		<a href={prev.slug} class="prev"><MoveLeftIcon />{prev.title}</a>
+<svelte:window bind:scrollY={y} on:scroll={handleScroll} />
+
+<div class="nav-container">
+	{#if isVisible}
+		<nav transition:slide={{ duration: 500 }}>
+			{#if prev}
+				<a href={prev.slug} class="prev"><MoveLeftIcon />{prev.title}</a>
+			{/if}
+			{#if next}
+				<a href={next.slug} class="next">{next.title}<MoveRightIcon /></a>
+			{/if}
+		</nav>
 	{/if}
-	{#if next}
-		<a href={next.slug} class="next">{next.title}<MoveRightIcon /></a>
-	{/if}
-</nav>
+</div>
+
 <div class="article-layout">
 	<article>
 		<slot />
@@ -22,11 +56,18 @@
 </div>
 
 <style>
-	nav {
-		display: flex;
+	.nav-container {
 		position: sticky;
 		top: 0;
 		z-index: 100;
+		height: 0;
+	}
+
+	nav {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
 		padding: 3rem 0;
 		margin-top: -1rem;
 		background-color: var(--bg-subtle);
@@ -63,6 +104,7 @@
 	.article-layout {
 		display: grid;
 		grid-template-columns: minmax(0, 1fr) minmax(auto, 50rem) minmax(0, 1fr);
+		padding-top: 7rem;
 	}
 
 	article {
