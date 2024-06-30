8a8c5874380b752dccc29c34375adff066aeca62
Moiri Gamboni <moiri.gamboni@proton.me>
2024-06-30 18:38:50 +0200

    fix: remove unnecessary risque article markup and styles

--- src/routes/risques/[slug]/+page.svelte
+++ src/routes/risques/[slug]/+page.svelte
@@ -18,44 +18,7 @@
 		{/if}
 	</hgroup>
 
-	<div class="tags">
-		{#if categories && categories.length > 0}
-			<div class="categories">
-				{#each categories as category}
-					<span class="surface-4">&num;{category}</span>
-				{/each}
-			</div>
-		{/if}
-	</div>
-
 	<div class="prose">
 		<svelte:component this={content} />
 	</div>
 </article>
-
-<style>
-	article {
-		max-inline-size: var(--size-content-3);
-		margin-inline: auto;
-	}
-
-	h1 {
-		text-transform: capitalize;
-	}
-
-	h1 + p {
-		margin-top: var(--size-2);
-		color: var(--text-2);
-	}
-
-	.tags {
-		display: flex;
-		gap: var(--size-3);
-		margin-top: var(--size-7);
-	}
-
-	.tags > * {
-		padding: var(--size-2) var(--size-3);
-		border-radius: var(--radius-round);
-	}
-</style>
