75ccfd085aa234bd5acfd3ec478e39a07802da42
Moiri Gamboni <moiri.gamboni@proton.me>
2024-06-30 18:38:50 +0200

    style: add icons

--- src/routes/risques/+layout.svelte
+++ src/routes/risques/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { MoveLeftIcon, MoveRightIcon } from 'lucide-svelte'
 	export let data
 	const index = data.posts.findIndex((post) => post.slug === data.url)
 	const prev = data.posts[index - 1]
@@ -7,11 +8,14 @@
 
 <nav>
 	{#if prev}
-		<a href={prev.slug}>Previous: {prev.title}</a>
+		<a href={prev.slug} class="prev"><MoveLeftIcon />{prev.title}</a>
 	{/if}
 	{#if next}
-		<a href={next.slug}>Next: {next.title}</a>
+		<a href={next.slug} class="next"><MoveRightIcon />{next.title}</a>
 	{/if}
 </nav>
 
 <slot></slot>
+
+<style>
+</style>
