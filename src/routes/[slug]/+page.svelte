fe447346c8979794528b53f3ec6177b3f063bf77
Moiri Gamboni <moiri.gamboni@proton.me>
2024-07-07 13:43:49 +0200

    style: update generic page layout and style

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import PostMeta from '$lib/components/PostMeta.svelte'
+	import UnderlinedTitle from '$lib/components/UnderlinedTitle.svelte'
 
 	export let data
 
@@ -11,25 +12,12 @@
 <PostMeta {title} {description} {date} {image} />
 
 <article>
-	{#if parent}
-		<a href={`/${parent}`}>View all {parent}</a>
-	{/if}
 	<hgroup>
-		<h1>{title}</h1>
+		<UnderlinedTitle>{title}</UnderlinedTitle>
 		{#if date}
 			<!-- <p>Published at {formatDate(date)}</p> -->
 		{/if}
 	</hgroup>
-
-	<div class="tags">
-		{#if categories && categories.length > 0}
-			<div class="categories">
-				{#each categories as category}
-					<span class="surface-4">&num;{category}</span>
-				{/each}
-			</div>
-		{/if}
-	</div>
 
 	<div class="prose">
 		<svelte:component this={content} />
@@ -38,27 +26,8 @@
 
 <style>
 	article {
-		max-inline-size: var(--size-content-3);
+		max-inline-size: 50rem;
 		margin-inline: auto;
-	}
-
-	h1 {
-		text-transform: capitalize;
-	}
-
-	h1 + p {
-		margin-top: var(--size-2);
-		color: var(--text-2);
-	}
-
-	.tags {
-		display: flex;
-		gap: var(--size-3);
-		margin-top: var(--size-7);
-	}
-
-	.tags > * {
-		padding: var(--size-2) var(--size-3);
-		border-radius: var(--radius-round);
+		margin-top: 3rem;
 	}
 </style>
