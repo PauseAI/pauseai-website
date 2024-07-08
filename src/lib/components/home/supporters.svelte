af0030eee5bbad7793e1a0778ad86b639e98da48
imbjdd <b0bb1e@protonmail.com>
2024-07-08 22:54:36 +0200

    fix: responsive

--- src/lib/components/home/supporters.svelte
+++ src/lib/components/home/supporters.svelte
@@ -109,8 +109,27 @@
 		position: relative;
 		display: flex;
 		gap: 1rem;
-		margin-left: -6rem;
-		margin-right: -6rem;
+		margin-left: -1rem;
+		margin-right: -1rem;
+	}
+	@media (min-width: 640px) {
+		.logos {
+			margin-left: -2rem;
+			margin-right: -2rem;
+		}
+	}
+
+	@media (min-width: 768px) {
+		.logos {
+			margin-left: -4rem;
+			margin-right: -4rem;
+		}
+	}
+	@media (min-width: 1024px) {
+		.logos {
+			margin-left: -6rem;
+			margin-right: -6rem;
+		}
 	}
 
 	.logo_items {
