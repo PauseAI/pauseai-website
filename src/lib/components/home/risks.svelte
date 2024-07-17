958a06ca8f1b99a81d5928a80574c42176824346
Moiri Gamboni <moiri.gamboni@proton.me>
2024-07-17 22:11:04 +0200

    content: risks tablist descriptions

--- src/lib/components/home/risks.svelte
+++ src/lib/components/home/risks.svelte
@@ -42,61 +42,59 @@
 				<p>
 					Imaginez-vous sur une application de rencontre, échangeant des messages avec ce qui semble
 					être la personne parfaite. Mais êtes-vous vraiment en train de discuter avec un être
-					humain ? Aujourd'hui, la réponse peut sembler évidente, car les bots sont parfois
-					maladroits. Mais cette réalité est sur le point de changer radicalement. La technologie
-					permettant de créer des conversations indiscernables d'interactions humaines existe déjà,
-					comme le démontrent les récents tests de Turing réussis. Elle pourrait être largement
-					déployée et rendre impossible la distinction entre une conversation avec un humain de
-					celle avec une IA. Cette technologie n'est pas un concept futuriste lointain ; elle existe
-					déjà et est en cours d'implémentation dans de nombreux domaines de notre vie numérique.
+					humain ? Aujourd'hui, la réponse peut sembler évidente, mais cette réalité est sur le
+					point de changer radicalement. La technologie permettant de créer des conversations
+					indiscernables d'interactions humaines existe déjà. Elle sera bientôt déployée et rendra
+					impossible toute distinction entre un humain et une IA.
 				</p>
 				<p>
-					Cette évolution rapide révèle à quel point l’IA s’immisce dans notre quotidien, menaçant
+					Cette évolution rapide révèle à quel point l’IA s’immisce dans notre quotidien, menace
 					notre intimité, notre sécurité et notre bien-être. Chaque jour, nous risquons de voir
-					notre vie privée violée et nos opinions manipulées par la désinformation. Les conséquences
-					peuvent être dévastatrices, tant sur le plan émotionnel que physique. Pour nous protéger,
-					il est crucial de mettre en place des cadres réglementaires solides, d’éduquer le public
-					et d’intégrer des gardes-fous dans les intelligences artificielles.
+					notre vie privée violée et nos opinions manipulées par la désinformation : les
+					conséquences seraient dévastatrices.
 				</p>
 				<Button href="/dangers/pour-les-individus">En apprendre davantage</Button>
 			{:else if tab === 'Pour la société'}
 				<p>
-					Dans un avenir proche, peut-être d'ici deux ans seulement, les intelligences artificielles
-					seront capables de concevoir des virus plus mortels que tout ce que nous connaissons
-					aujourd'hui. Le bioterrorisme assisté par l'IA n'est qu'un exemple des menaces émergentes.
-					Parallèlement, le risque de cyberattaques dévastatrices, orchestrées ou amplifiées par des
-					IA sophistiquées, plane sur nos infrastructures critiques et nos systèmes de
-					communication.
+					Selon la majorité des experts, les intelligences artificielles seront capables de
+					concevoir et synthétiser à très court terme des virus plus mortels que tout ce que nous
+					connaissons aujourd'hui. Le bioterrorisme assisté par l'IA n'est qu'un exemple parmi une
+					multitude de menaces émergentes. Le risque de cyberattaques dévastatrices, orchestrées ou
+					amplifiées par des IA sophistiquées, plane sur nos infrastructures critiques et nos
+					systèmes de communication.
 				</p>
 				<p>
 					Ces scénarios ne sont que la partie visible d'un danger bien plus large : l'IA menace le
 					cœur même de notre société. Elle touche à la cohésion sociale et aux fondements de notre
 					démocratie. Nos processus démocratiques sont fragilisés par la polarisation et la
 					désinformation. Les biais raciaux s'exacerbent, l’automatisation des emplois se profile,
-					et l'ombre de nouvelles formes de guerre et de terrorisme plane sur nous. Ces dangers
-					peuvent détruire nos valeurs et nos structures sociales.
-				</p>
-				<p>
-					Pour les contrer, nous devons urgemment mettre en place une régulation proactive, repenser
-					nos systèmes d'information et promouvoir la pensée critique.
+					et l'ombre de nouvelles formes de guerre et de terrorisme plane sur nous.
 				</p>
 				<Button href="/dangers/pour-la-societe">En apprendre davantage</Button>
 			{:else if tab === 'Pour l’humanité'}
 				<p>
-					L'IA représente la plus grande menace jamais connue pour le futur de l'humanité. OpenAI,
-					DeepMind, Anthropic et d'autres géants technologiques investissent des milliards de
-					dollars dans la création d'une IA aux capacités quasi divines. Leurs dirigeants affirment
-					qu'ils pourraient réussir dans les prochaines années. Or, ils admettent ne pas savoir
-					comment contrôler leur création et reconnaissent que l'humanité pourrait s'éteindre à
-					cause de celle-ci.
+					OpenAI, DeepMind, Anthropic et d'autres géants technologiques investissent des milliards
+					de dollars dans la création d'une IA aux capacités surhumaines. Leurs dirigeants affirment
+					qu'ils pourraient réussir dans les quatres prochaines années. Ils admettent ne pas savoir
+					comment contrôler leur création et reconnaissent publiquement que l'humanité pourrait
+					s'éteindre à cause de celle-ci.
 				</p>
 				<p>
-					La perte de contrôle des intelligences artificielles ou leur désalignement sont des
-					dangers qui ne peuvent pas forcément être observés, et il est peu probable que nous aurons
-					des sirènes d’alarme plus claires qu’aujourd’hui avant qu’il ne soit trop tard. Le
-					potentiel extraordinaire de l'IA générale pourrait se révéler apocalyptique. Notre seul
-					espoir réside en une pause qui favorise la recherche en alignement technique.
+					Les plus éminents experts en IA ont signé une lettre ouverte avertissant que "L'IA
+					représente un risque existentiel pour l'humanité, au même titre que les pandémies et les
+					guerres nucléaires."
 				</p>
+				<p>
+					Les chercheurs spécialisés en sécurité de l'IA estiment à 30% la probabilité d'une
+					extinction de l'humanité causée par l'IA.
+				</p>
+				<p>
+					Jamais auparavant l'humanité n'a fait face à une menace aussi imminente et potentiellement
+					catastrophique. Contrairement à d'autres défis, la crise de l'IA se distingue par son
+					extrême urgence et son potentiel de transformation radicale à très court terme, suscitant
+					une mobilisation sans précédent des plus grands esprits de notre époque.
+				</p>
+				<p>Allons-nous attendre une catastrophe mondiale pour réagir ?</p>
 				<Button href="/dangers/pour-l'humanite">En apprendre davantage</Button>
 			{/if}
 		</svelte:fragment>
