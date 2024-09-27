7e34eed11209e3cc1c1ab63bb92ae7aa266c9322
Joep Meindertsma <joep@ontola.io>
2024-09-27 09:49:03 +0200

    Cache people page

--- src/routes/people/+page.ts
+++ src/routes/people/+page.ts
@@ -1,11 +1,12 @@
 import type { Person } from '$lib/types'
 
-export const prerender = false
+export const prerender = true
 
 export const load = async ({ fetch }) => {
 	const response = await fetch('api/people')
 	const people: Person[] = await response.json()
 	return {
-		people: people
+		people: people,
+		maxage: 3600 // 1 hour in seconds
 	}
 }
