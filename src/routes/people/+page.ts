d66ae8f57c1ebe86056b61cdf3ee1996c961798b
Joep Meindertsma <joep@ontola.io>
2024-09-27 09:58:14 +0200

    auto

--- src/routes/people/+page.ts
+++ src/routes/people/+page.ts
@@ -1,12 +1,12 @@
 import type { Person } from '$lib/types'
 
-export const prerender = true
+export const prerender = 'auto'
 
 export const load = async ({ fetch }) => {
 	const response = await fetch('api/people')
 	const people: Person[] = await response.json()
 	return {
 		people: people,
-		maxage: 120 // 1 hour in seconds
+		maxage: 60 // 1 hour in seconds
 	}
 }
