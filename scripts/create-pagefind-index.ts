461d1a785ea44fc93e80d67b656053bfaa18e401
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-10-06 01:50:12 +0200

    Revert "Index dynamic routes using Pagefind"
    This reverts commit 08dd998ca0e8b3d0bf3d9b0e69a996b502775403.

--- scripts/create-pagefind-index.ts
+++ scripts/create-pagefind-index.ts
@@ -1,31 +1,9 @@
 import { promises as fs } from 'fs'
 import * as pagefind from 'pagefind'
-import type { Post } from '../src/lib/types'
-
-const POSTS_PATH = 'build/api/posts'
-const INPUT_PATH = 'build'
-const STATIC_PATH = 'static/pagefind'
-const BUILD_PATH = 'build/pagefind'
 
 const { index, errors } = await pagefind.createIndex({})
 if (!index) throw new Error(errors.toString())
+await index.addDirectory({ path: 'build' })
+await index.writeFiles({ outputPath: 'static/pagefind' })
 
-const posts: Post[] = JSON.parse(await fs.readFile(POSTS_PATH, 'utf-8'))
-for (const post of posts) {
-	await index.addCustomRecord({
-		url: '/' + post.slug,
-		content: post.description ?? '',
-		language: 'en',
-		meta: {
-			title: post.title
-		}
-	})
-}
-
-await index.addDirectory({ path: INPUT_PATH })
-
-await index.writeFiles({ outputPath: STATIC_PATH })
-
-await pagefind.close()
-
-await fs.cp(STATIC_PATH, BUILD_PATH, { recursive: true })
+await fs.cp('static/pagefind', 'build/pagefind', { recursive: true })
