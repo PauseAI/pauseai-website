234176cfb5b8ec0ffb459f62b95a257a88e77db1
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-10-06 00:31:00 +0200

    Cache people page

--- src/routes/people/+page.ts
+++ src/routes/people/+page.ts
@@ -2,9 +2,15 @@ import type { Person } from '$lib/types'
 
 export const prerender = false
 
-export const load = async ({ fetch }) => {
+export const load = async ({ fetch, setHeaders }) => {
 	const response = await fetch('api/people')
 	const people: Person[] = await response.json()
+	const cacheControlHeader = response.headers.get('cache-control')
+	if (cacheControlHeader) {
+		setHeaders({
+			'cache-control': cacheControlHeader
+		})
+	}
 	return {
 		people: people
 	}
