da84c5ff2b476782cc58ee9ad9f8c9f747e07768
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-10-09 15:57:23 +0200

    Add Paraglide

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 
 const config = {
 	server: {
 		port: 37572
 	},
-	plugins: [sveltekit()]
+	plugins: [
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		}),
+		sveltekit()
+	]
 }
 
 export default config
