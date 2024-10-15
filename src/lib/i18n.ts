61b248b5924e80b2e20384265f9dee54f01a9dc5
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-10-15 22:55:02 +0200

    Fix build

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,7 +2,12 @@
 import { createI18n } from '@inlang/paraglide-sveltekit'
 import * as runtime from '$lib/paraglide/runtime.js'
 
+const STATIC_PATH_PREFIX = '../../static'
+const staticFiles = import.meta.glob('../../static/*', {
+	query: '?raw'
+})
+
 export const i18n = createI18n(runtime, {
 	prefixDefaultLanguage: 'always',
-	exclude: [/[^/]+\.[^/]+$/] // static files and api
+	exclude: Object.keys(staticFiles).map((path) => path.substring(STATIC_PATH_PREFIX.length))
 })
