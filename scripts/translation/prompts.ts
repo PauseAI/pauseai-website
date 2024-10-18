6d519068428569cdb31b3886b8f0406bc80321a8
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-10-18 22:51:45 +0200

    WIP

--- scripts/translation/prompts.ts
+++ scripts/translation/prompts.ts
@@ -1,7 +1,7 @@
-export function generateJsonPrompt(languageName: string, content: string) {
-	return `You are a professional translator who specializes in translating to ${languageName}. Therefore, you don't make basic errors with regard to spelling and grammar.
+export type PromptGenerator = (languageName: string, content: string) => string
 
-Translate the following JSON content to ${languageName}. Avoid translating word for word whenever possible. Commons phrases should be adapted to the most fitting counterpart in the target language or a less figurative wording if nothing matches well. The content goes on an informative website of an activist group with the aim of achieving global coordination to stop the development of artificial intelligence systems more powerful than GPT-4. Never use histrionic language that could associate the concerns with fiction. Instead, use neutral scientific language that is still easy to understand and compelling.
+export const generateJsonPrompt: PromptGenerator = (languageName: string, content: string) => {
+	return `${commonHead(languageName, 'JSON')}
 
 Maintain the JSON structure and only translate the values, not the keys. Keys prefixed with an underscore provide additional instructions for specific translations. Ensure the result is valid JSON syntax:
 
@@ -9,4 +9,21 @@ ${content}
 
 Do not start with \`\`\`json, just return the JSON.
 Translated JSON:`
+}
+
+export const generateMarkdownPrompt: PromptGenerator = (languageName: string, content: string) => {
+	return `${commonHead(languageName, 'Markdown')}
+
+Maintain the structure of the document and don't translate link HTML tags, link targets and file names. Ensure the result is valid Markdown syntax:
+
+${content}
+
+Do not start with \`\`\`md, just return the Markdown.
+Translated Markdown:`
+}
+
+function commonHead(languageName: string, format: string) {
+	return `You are a professional translator who specializes in translating to ${languageName}. Therefore, you don't make basic errors with regard to spelling and grammar.
+
+Translate the following ${format} content to ${languageName}. Avoid translating word for word whenever possible. Commons phrases should be adapted to the most fitting counterpart in the target language or a less figurative wording if nothing matches well. Use your judgement to determine if keeping the English word is an appropriate alternative in the target language if there is no fitting translation. The content goes on an informative website of an activist group with the aim of achieving global coordination to stop the development of artificial intelligence systems more powerful than GPT-4. Never use histrionic language that could associate the concerns with fiction. Instead, use neutral scientific language that is still easy to understand and compelling.`
 }
--- scripts/translation/translate.ts
+++ scripts/translation/translate.ts
@@ -1,51 +1,158 @@
+import { createClient } from '@supabase/supabase-js'
+import { hash } from 'crypto'
+import 'dotenv/config'
 import fs from 'fs/promises'
 import OpenAI from 'openai'
-import 'dotenv/config'
-import { generateJsonPrompt } from './prompts'
-import path from 'path'
+import { join, relative, dirname } from 'path'
+import PQueue from 'p-queue'
 import inlangSettings from '../../project.inlang/settings.json'
-import { TaskQueue } from './utils.ts'
+import { generateJsonPrompt, generateMarkdownPrompt, PromptGenerator } from './prompts'
+import { existsSync } from 'fs'
 
-const PATH_JSON = './messages'
+const PATH_JSON_BASE = './messages'
 const PATH_JSON_SOURCE = './messages/en.json'
 const BASE_URL = 'https://api.sambanova.ai/v1'
 const API_KEY = process.env.SAMBANOVA_API_KEY
 const MODEL = 'Meta-Llama-3.1-405B-Instruct'
+const JSON_PATH_IN_CACHE = '_messages.json'
+const SUPABASE_URL = 'http://127.0.0.1:54321'
+const PATH_MD_BASE = 'src/posts'
+const PATH_MD_TARGET = 'temp/translations'
 
-const languages = inlangSettings.languageTags
-const indexOfSourceLanguage = languages.indexOf(inlangSettings.sourceLanguageTag)
-languages.splice(indexOfSourceLanguage, 1)
-
-const queue = new TaskQueue(1000)
-
-const languageNamesInEnglish = new Intl.DisplayNames('en', { type: 'language' })
-
+const queue = new PQueue({
+	concurrency: 1,
+	intervalCap: 1,
+	interval: 10000
+})
 const openai = new OpenAI({
 	baseURL: BASE_URL,
 	apiKey: API_KEY
 })
+const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_API_KEY as string)
+const languageNamesInEnglish = new Intl.DisplayNames('en', { type: 'language' })
 
-const messagesJson = await fs.readFile(PATH_JSON_SOURCE, 'utf-8')
+{
+	const languages = inlangSettings.languageTags
+	const indexOfSourceLanguage = languages.indexOf(inlangSettings.sourceLanguageTag)
+	languages.splice(indexOfSourceLanguage, 1)
 
-await Promise.all(
-	languages.map(async (language) => {
-		const languageName = languageNamesInEnglish.of(language)
-		if (!languageName) throw new Error(`Couldn't resolve language code: ${language}`)
-		const prompt = generateJsonPrompt(languageName, messagesJson)
-		const response = await queue.addTask(() =>
-			openai.chat.completions.create({
-				model: MODEL,
-				messages: [
-					{
-						role: 'user',
-						content: prompt
+	const markdownPathsFromBase = await fs.readdir(PATH_MD_BASE, { recursive: true })
+	const markdownPathsFromRoot = markdownPathsFromBase.map((file) => join(PATH_MD_BASE, file))
+
+	await Promise.all([
+		translateOrLoadMessages(PATH_JSON_SOURCE, PATH_JSON_BASE, JSON_PATH_IN_CACHE, languages),
+		translateOrLoadMarkdown(markdownPathsFromRoot, PATH_MD_BASE, languages, PATH_MD_TARGET)
+	])
+}
+
+async function translateOrLoadMessages(
+	path: string,
+	basePath: string,
+	pathInCache: string,
+	languages: string[]
+) {
+	return await translateOrLoad(
+		[path],
+		() => pathInCache,
+		languages,
+		generateJsonPrompt,
+		(language) => join(basePath, language + '.json')
+	)
+}
+
+async function translateOrLoadMarkdown(
+	paths: string[],
+	basePath: string,
+	languages: string[],
+	target: string
+) {
+	return await translateOrLoad(
+		paths,
+		(path) => relative(basePath, path),
+		languages,
+		generateMarkdownPrompt,
+		(language, path) => {
+			const relativePath = relative(basePath, path)
+			return join(target, language, relativePath)
+		}
+	)
+}
+
+type CacheNamingStrategy = (path: string) => string
+type TargetStrategy = (language: string, path: string) => string
+
+async function translateOrLoad(
+	paths: string[],
+	cacheNamingStrategy: CacheNamingStrategy,
+	languages: string[],
+	promptGenerator: PromptGenerator,
+	targetStrategy: TargetStrategy
+) {
+	// for (path of paths) {
+	await Promise.all(
+		paths.map(async (path) => {
+			const content = await fs.readFile(path, 'utf-8')
+			const hashedContent = hash('md5', content)
+			// for (language of languages) {
+			await Promise.all(
+				languages.map(async (language) => {
+					const pathInCache = cacheNamingStrategy(path)
+					const cached = await supabase.from('translation').select('translation').match({
+						path: pathInCache,
+						hash: hashedContent,
+						language_code: language
+					})
+					let translated
+					if (cached.data?.length) {
+						console.log(`Using up-to-date translation from cache for ${pathInCache} in ${language}`)
+						translated = cached.data[0].translation
+					} else {
+						translated = await translate(content, promptGenerator, language)
+						const idResponse = await supabase.from('translation').select('id').match({
+							path: pathInCache,
+							language_code: language
+						})
+						const id = idResponse.data?.[0]?.id
+						if (id)
+							console.log(
+								`Updating outdated translation in cache for ${pathInCache} in ${language}`
+							)
+						else console.log(`Creating new cache entry for ${pathInCache} in ${language}`)
+						await supabase.from('translation').upsert({
+							id: id,
+							path: pathInCache,
+							hash: hashedContent,
+							language_code: language,
+							translation: translated
+						})
 					}
-				],
-				temperature: 0
-			})
-		)
-		const translated = response.choices[0].message.content
-		if (!translated) throw new Error(`Translation to ${language} failed`)
-		await fs.writeFile(path.join(PATH_JSON, language + '.json'), translated)
-	})
-)
+					const target = targetStrategy(language, path)
+					const directory = dirname(target)
+					if (!existsSync(directory)) await fs.mkdir(directory, { recursive: true })
+					await fs.writeFile(targetStrategy(language, path), translated)
+				})
+			)
+		})
+	)
+}
+
+async function translate(content: string, promptGenerator: PromptGenerator, language: string) {
+	const languageName = languageNamesInEnglish.of(language)
+	if (!languageName) throw new Error(`Couldn't resolve language code: ${language}`)
+	const prompt = promptGenerator(languageName, content)
+	const response = await queue.add(() =>
+		openai.chat.completions.create({
+			model: MODEL,
+			messages: [
+				{
+					role: 'user',
+					content: prompt
+				}
+			],
+			temperature: 0
+		})
+	)
+	const translated = response?.choices[0].message.content
+	if (!translated) throw new Error(`Translation to ${languageName} failed`)
+	return translated
+}
