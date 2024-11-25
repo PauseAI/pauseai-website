7b0316086669570356673357f3e2948fcfe9ecce
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-11-25 11:55:10 +0100

    Cache paraglide redirect

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,7 @@ import { building } from '$app/environment'
 import { i18n } from '$lib/i18n'
 import { redirect, type Handle } from '@sveltejs/kit'
 import * as runtime from '$lib/paraglide/runtime'
+import { sequence } from '@sveltejs/kit/hooks'
 
 const LANG_PLACEHOLDER = '%paraglide.lang%'
 const DIRECTION_PLACEHOLDER = '%paraglide.textDirection%'
@@ -12,7 +13,7 @@ const paraglideHandle = i18n.handle({
 	textDirectionPlaceholder: DIRECTION_PLACEHOLDER
 })
 
-export const handle: Handle = async function ({ event, resolve }) {
+const emulateOrCallParaglide: Handle = async function ({ event, resolve }) {
 	// emulate Paraglide hook without requesting headers etc
 	if (building) {
 		const pathname = event.url.pathname
@@ -22,7 +23,7 @@ export const handle: Handle = async function ({ event, resolve }) {
 			redirect(302, i18n.resolveRoute(pathname, i18n.config.defaultLanguageTag))
 		}
 		runtime.setLanguageTag(languageFromUrl)
-		//@ts-ignore
+		//@ts-expect-error types assume browser environment, node differs
 		const direction = new Intl.Locale(languageFromUrl).textInfo.direction
 		return await resolve(event, {
 			transformPageChunk({ html, done }) {
@@ -36,3 +37,22 @@ export const handle: Handle = async function ({ event, resolve }) {
 		return paraglideHandle({ event, resolve })
 	}
 }
+
+const addCacheHeaderToParaglideRedirect: Handle = async function ({ event, resolve }) {
+	const response = await resolve(event)
+	if (isParaglideRedirect(event, response)) {
+		response.headers.set('cache-control', 'public, max-age=3600') // 1 hour in seconds
+	}
+	return response
+}
+
+function isParaglideRedirect(event: Parameters<Handle>[0]['event'], response: Response) {
+	if (response.status != 302) return false
+	const location = response.headers.get('location')
+	if (!location) return false
+	const canonical = i18n.route(location)
+	if (canonical != event.url.pathname) return false
+	return true
+}
+
+export const handle = sequence(addCacheHeaderToParaglideRedirect, emulateOrCallParaglide)
