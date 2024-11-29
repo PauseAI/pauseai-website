6bfdaf592ef7f2d23dc1050cc410dd9ad307e5b2
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-11-29 13:27:43 +0100

    Revert "Use adapter netlify"
    This reverts commit 83d2b320a929083196f215d128a6de8a446363d4.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
-import netlifyAdapter from '@sveltejs/adapter-netlify'
 
 const config = {
-	kit: {
-		adapter: netlifyAdapter()
-	},
 	server: {
 		port: 37572
 	},
