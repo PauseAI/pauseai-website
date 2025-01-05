f286f33b0e50196360474bdeee31198ad1609749
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-01-05 17:22:32 +0100

    Fix meta 2

--- src/routes/pfp/meta.ts
+++ src/routes/pfp/meta.ts
@@ -2,7 +2,7 @@ import type { Post } from '$lib/types'
 
 export const meta: Post = {
 	title: 'Profile picture generator',
-	slug: 'pfpgen',
+	slug: 'pfp',
 	description:
 		'Upload your profile picture to generate an image with the date of our global protest and the address of the corresponding website.',
 	date: '2024-05-05',
