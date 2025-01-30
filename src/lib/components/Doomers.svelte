7c45a20c5eed61861b2e70d4510e378c2f58adf1
Joep Meindertsma <joep@ontola.io>
2025-01-30 11:01:50 +0100

    Add nate silvers pdoom

--- src/lib/components/Doomers.svelte
+++ src/lib/components/Doomers.svelte
@@ -28,6 +28,13 @@
 				'https://static1.squarespace.com/static/635693acf15a3e2a14a56a4a/t/64f0a7838ccbf43b6b5ee40c/1693493128111/XPT.pdf'
 		},
 		{
+			name: 'Nate Silver',
+			title: 'Founder of FiveThirtyEight',
+			probability: '5-10%',
+			number: 0.075,
+			source: 'https://www.natesilver.net/p/its-time-to-come-to-grips-with-ai'
+		},
+		{
 			name: 'Vitalik Buterin',
 			title: 'Ethereum founder',
 			probability: '10%',
