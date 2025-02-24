bc0a5dd13e2b7f1a9e214a49d9d741cf2c68b062
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-02-24 23:04:22 +0100

    Fix mix of locales(?)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,16 @@
 export const prerender = true
-import { redirect } from '@sveltejs/kit'
 
-export async function load({ url: { host, pathname: url } }) {
+import { redirect } from '@sveltejs/kit'
+import { i18n } from '$lib/i18n'
+
+export async function load({ url, url: { host, pathname } }) {
+	// load functions run before wrapper component which normally sets language tag for client
+	const language = i18n.getLanguageFromUrl(url)
+	const runtime = i18n.config.runtime
+	runtime.setLanguageTag(language)
+
 	if (host === 'pauseai.org') {
-		return redirect(301, 'https://pauseai.info' + url)
+		return redirect(301, 'https://pauseai.info' + pathname)
 	}
-	return { url }
+	return { url: pathname }
 }
