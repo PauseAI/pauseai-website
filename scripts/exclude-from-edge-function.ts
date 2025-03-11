cfba81aae3ff759f74280aee21f5671dbc1db07e
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-03-11 17:08:31 +0100

    Fix compatibility with Netlify adapter

--- scripts/exclude-from-edge-function.ts
+++ scripts/exclude-from-edge-function.ts
@@ -1,9 +1,9 @@
 import { readEdgeManifest, searchRenderFunction, writeEdgeManifest } from './utils'
 
-const EXCLUDE_PATHS = ['^/pagefind/.*$', '^/~partytown/.*$']
+const EXCLUDE_PATHS = ['/pagefind/*']
 
 const manifest = await readEdgeManifest()
 const renderFunction = await searchRenderFunction(manifest)
-if (!renderFunction.excludedPattern) renderFunction.excludedPattern = []
-renderFunction.excludedPattern = EXCLUDE_PATHS.concat(renderFunction.excludedPattern)
+if (!renderFunction.excludedPath) renderFunction.excludedPath = []
+renderFunction.excludedPath = EXCLUDE_PATHS.concat(renderFunction.excludedPath)
 await writeEdgeManifest(manifest)
