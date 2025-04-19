d622a06ee6ae27b71c4d9525907df0276b102bb0
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-04-19 18:26:25 +0200

    Don't prerender geo endpoint

--- src/routes/api/geo/+server.ts
+++ src/routes/api/geo/+server.ts
@@ -1,5 +1,7 @@
 import type { Platform } from '$lib/netlify'
 
+export const prerender = false
+
 export function GET({ platform }) {
 	const netlifyPlatform = platform as Readonly<Platform>
 	const geo = netlifyPlatform.context.geo
