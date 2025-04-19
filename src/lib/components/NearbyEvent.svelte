3728454c327ca01231d329174a725daf912d5723
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-04-19 18:25:30 +0200

    Add nearby event component

---
+++ src/lib/components/NearbyEvent.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type GetItems from '$lib/clients/luma/types/calendar/get-items'
+	import type { Event } from '$lib/clients/luma/types/calendar/get-items'
+	import distance from '@turf/distance'
+	import { onMount } from 'svelte'
+	import type { Platform } from '../netlify'
+	import Banner from './Banner.svelte'
+	import ExternalLink from '$lib/components/custom/a.svelte'
+
+	const MAX_DISTANCE_KM = 100
+
+	let nearbyEvent: Event | null = null
+
+	onMount(async () => {
+		const [geo, events] = await Promise.all([
+			fetch('/api/geo').then((res) => res.json()) as Promise<Platform['context']['geo']>,
+			fetch('/api/luma').then((res) => res.json()) as Promise<GetItems>
+		])
+
+		const { latitude: userLatitude, longitude: userLongitude } = geo
+		if (!userLatitude || !userLongitude) return
+
+		const userCoords = [userLatitude, userLongitude]
+
+		const isNearby = (event: Event): boolean => {
+			const { geo_latitude, geo_longitude } = event
+			if (!geo_latitude || !geo_longitude) return false
+			const eventCoords = [geo_latitude, geo_longitude].map(Number.parseFloat)
+			return distance(userCoords, eventCoords, { units: 'kilometers' }) <= MAX_DISTANCE_KM
+		}
+
+		nearbyEvent = events.entries.map((entry) => entry.event).find(isNearby) ?? null
+	})
+</script>
+
+{#if nearbyEvent}
+	<Banner>
+		Next up in your area: <ExternalLink href={nearbyEvent.url}>{nearbyEvent.name}</ExternalLink> on {nearbyEvent.start_at.toDateString()}
+	</Banner>
+{/if}
