30d312a0f76f2c5b9521da818e08ee445450a5d6
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-04-22 17:45:08 +0200

    Cache geo endpoint locally

--- src/routes/api/geo/+server.ts
+++ src/routes/api/geo/+server.ts
@@ -2,8 +2,11 @@ import type { Platform } from '$lib/netlify'
 
 export const prerender = false
 
-export function GET({ platform }) {
+export function GET({ platform, setHeaders }) {
 	const netlifyPlatform = platform as Readonly<Platform>
 	const geo = netlifyPlatform.context.geo
+	setHeaders({
+		'cache-control': 'private, max-age=3600' // local only, 1 hour in seconds
+	})
 	return new Response(JSON.stringify(geo))
 }
