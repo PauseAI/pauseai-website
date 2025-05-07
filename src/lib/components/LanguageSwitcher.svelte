7b9207550eb5dbd779980e5b57b3416fd8ee32d4
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-05-07 11:56:00 +0200

    Remove gap from navbar #317

--- src/lib/components/LanguageSwitcher.svelte
+++ src/lib/components/LanguageSwitcher.svelte
@@ -143,7 +143,7 @@
 	}
 </script>
 
-<div class:hidden={!showSwitcher}>
+{#if showSwitcher}
 	<Navlink {inverted} narrow active={open}>
 		<button
 			class="button reset-button"
@@ -156,7 +156,7 @@
 			<Globe size="0.9em" />&nbsp;{getLocale().toUpperCase()}
 		</button>
 	</Navlink>
-</div>
+{/if}
 {#if (open || building) && showSwitcher}
 	<div class="dropdown" bind:this={dropdown}>
 		<Card>
