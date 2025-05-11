05b9b335c00e95ea2fce98e578afe2448fd02e9b
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-05-11 17:32:57 +0200

    Add email verification

---
+++ src/routes/api/verification/+server.ts
@@ -0,0 +1,25 @@
+export const prerender = false
+
+import Airtable from 'airtable'
+import { AIRTABLE_API_KEY } from '$env/static/private'
+
+const BASE_ID = 'appJI3O6GrPx1zrzY'
+const TABLE_ID = 'tblEMavUHON6r3rpL'
+const KEY_FIELD_NAME = 'Verification key'
+const VERIFIED_FIELD_NAME = 'Verified'
+
+const TABLE = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(BASE_ID).table(TABLE_ID)
+
+export async function GET({ url }) {
+	const key = url.searchParams.get('key')
+	if (!key) {
+		return new Response('Parameter "key" is required', { status: 400 })
+	}
+
+	const records = await TABLE.select({
+		filterByFormula: `{${KEY_FIELD_NAME}} = "${key}"`
+	}).firstPage()
+	await records[0]?.patchUpdate(Object.fromEntries([[VERIFIED_FIELD_NAME, true]]))
+
+	return new Response(null, { status: 301, headers: { Location: '/' } })
+}
