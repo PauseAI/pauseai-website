6c4e35d2582e27e03168491fb0e72920a2839adf
Patricio <patriciovercesi@hotmail.com>
2025-05-17 00:44:19 -0300

    form insertion

--- src/routes/petition/+page.svelte
+++ src/routes/petition/+page.svelte
@@ -13,6 +13,16 @@
 
 <h2>We call on the governments of the world to sign an international treaty implementing a temporary pause on the training of the most powerful general AI systems, until we know how to build them safely and keep them under democratic control.</h2>
 
+<iframe
+    class="airtable-embed"
+    src="https://airtable.com/embed/appWPTGqZmUcs3NWu/pagUTBbn9qea358FB/form"
+    frameborder="0"
+    width="100%"
+    height="533"
+    style="background: transparent; border: 1px solid #ccc;"
+></iframe>
+
+<h3>Signatories</h3>
 <section data-pagefind-ignore>
 	{#if signatories.length === 0}
 		<p>No signatories found</p>
