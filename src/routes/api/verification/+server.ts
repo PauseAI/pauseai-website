243d6804eb55be7b94f49880af3f9234cf887967
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-05-22 22:35:04 +0200

    Support multiple tables

--- src/routes/api/verification/+server.ts
+++ src/routes/api/verification/+server.ts
@@ -4,12 +4,15 @@ import Airtable from 'airtable'
 import { AIRTABLE_API_KEY } from '$env/static/private'
 import { verificationParameter } from '$lib/config.js'
 
-const BASE_ID = 'appJI3O6GrPx1zrzY'
-const TABLE_ID = 'tblEMavUHON6r3rpL'
 const KEY_FIELD_NAME = 'Verification key'
 const VERIFIED_FIELD_NAME = 'Verified'
+const TABLE_PARAMETER = 'table'
+const DEFAULT_TABLE = 'join'
 
-const TABLE = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(BASE_ID).table(TABLE_ID)
+const VERIFICATION_TABLES = new Map([
+	['join', { baseId: 'appJI3O6GrPx1zrzY', tableId: 'tblEMavUHON6r3rpL' }],
+	['statement', { baseId: 'appJI3O6GrPx1zrzY', tableId: 'tblEMavUHON6r3rpL' }]
+])
 
 export async function GET({ url }) {
 	const key = url.searchParams.get(verificationParameter)
@@ -17,9 +20,22 @@ export async function GET({ url }) {
 		return new Response(`Parameter "${verificationParameter}" is required`, { status: 400 })
 	}
 
-	const records = await TABLE.select({
-		filterByFormula: `{${KEY_FIELD_NAME}} = "${key}"`
-	}).firstPage()
+	const tableName = url.searchParams.get(TABLE_PARAMETER) || DEFAULT_TABLE
+	const tableConfig = VERIFICATION_TABLES.get(tableName)
+
+	if (!tableConfig) {
+		return new Response(`Invalid table name "${tableName}"`, { status: 400 })
+	}
+
+	const table = new Airtable({ apiKey: AIRTABLE_API_KEY })
+		.base(tableConfig.baseId)
+		.table(tableConfig.tableId)
+
+	const records = await table
+		.select({
+			filterByFormula: `{${KEY_FIELD_NAME}} = "${key}"`
+		})
+		.firstPage()
 	await records[0]?.patchUpdate(Object.fromEntries([[VERIFIED_FIELD_NAME, true]]))
 
 	return new Response(null, { status: 301, headers: { Location: '/' } })
