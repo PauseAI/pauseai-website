40da659d183a00cf8ab9a25b480962d842a41de1
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-05-22 22:58:08 +0200

    Add email verification (#326)
    * Add email verification
    
    * Add feedback
    
    * Support multiple tables
    
    * Configure Airtable key, table IDs

---
+++ src/routes/api/verification/+server.ts
@@ -0,0 +1,42 @@
+export const prerender = false
+
+import Airtable from 'airtable'
+import { AIRTABLE_WRITE_API_KEY } from '$env/static/private'
+import { verificationParameter } from '$lib/config.js'
+
+const KEY_FIELD_NAME = 'Verification key'
+const VERIFIED_FIELD_NAME = 'Verified'
+const TABLE_PARAMETER = 'table'
+const DEFAULT_TABLE = 'join'
+
+const VERIFICATION_TABLES = new Map([
+	['join', { baseId: 'appWPTGqZmUcs3NWu', tableId: 'tblL1icZBhTV1gQ9o' }],
+	['statement', { baseId: 'appWPTGqZmUcs3NWu', tableId: 'tbl2emfOWNWoVz1kW' }]
+])
+
+export async function GET({ url }) {
+	const key = url.searchParams.get(verificationParameter)
+	if (!key) {
+		return new Response(`Parameter "${verificationParameter}" is required`, { status: 400 })
+	}
+
+	const tableName = url.searchParams.get(TABLE_PARAMETER) || DEFAULT_TABLE
+	const tableConfig = VERIFICATION_TABLES.get(tableName)
+
+	if (!tableConfig) {
+		return new Response(`Invalid table name "${tableName}"`, { status: 400 })
+	}
+
+	const table = new Airtable({ apiKey: AIRTABLE_WRITE_API_KEY })
+		.base(tableConfig.baseId)
+		.table(tableConfig.tableId)
+
+	const records = await table
+		.select({
+			filterByFormula: `{${KEY_FIELD_NAME}} = "${key}"`
+		})
+		.firstPage()
+	await records[0]?.patchUpdate(Object.fromEntries([[VERIFIED_FIELD_NAME, true]]))
+
+	return new Response(null, { status: 301, headers: { Location: '/' } })
+}
