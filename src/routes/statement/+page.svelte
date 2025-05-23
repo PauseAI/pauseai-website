f405c8a21e0bc03f338c620b5469085c2b1cae59
Patricio <patriciovercesi@hotmail.com>
2025-05-23 02:29:02 -0300

    oops

--- src/routes/statement/+page.svelte
+++ src/routes/statement/+page.svelte
@@ -23,6 +23,7 @@
 	const nextGoal = milestones.find((goal) => totalCount < goal) || milestones[milestones.length - 1]
     
 	var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
+</script>
 
 <PostMeta {title} {description} {date} />
 
