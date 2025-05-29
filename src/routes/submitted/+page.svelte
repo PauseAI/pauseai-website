289f64cb1e02cb3c6c827331894334f3d3487e59
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-05-29 14:27:35 +0200

    Redirect to Stripe conditional on query parameter from Tally input

---
+++ src/routes/submitted/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { goto } from '$app/navigation'
+	import { page } from '$app/stores'
+	import { onMount } from 'svelte'
+
+	const STRIPE_PAYMENT_LINK_BASE_URL = 'YOUR_STRIPE_PAYMENT_LINK_BASE_URL'
+	const INTERNAL_REDIRECT_PATH = '/submitted'
+
+	onMount(() => {
+		const url = $page.url
+		const params = new URLSearchParams(url.search)
+		const redirectToPayment = params.get('payment') === 'true'
+
+		if (redirectToPayment) {
+			// Construct Stripe payment link with remaining parameters
+			params.delete('payment')
+			const stripePaymentLink = `${STRIPE_PAYMENT_LINK_BASE_URL}?${params.toString()}`
+			window.location.href = stripePaymentLink
+		} else {
+			// Redirect to internal page without query parameters
+			goto(INTERNAL_REDIRECT_PATH)
+		}
+	})
+</script>
+
+<h1>Processing your request...</h1>
