a8be31191ffa02ba566657545c7f578202c83263
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-05-30 22:54:14 +0200

    Fix query parameter

--- src/routes/submitted/+page.svelte
+++ src/routes/submitted/+page.svelte
@@ -5,11 +5,13 @@
 
 	const STRIPE_PAYMENT_LINK_BASE_URL = 'https://donate.stripe.com/aFadR9dAleJe3wO95ed7q00'
 	const INTERNAL_REDIRECT_PATH = '/'
+	const PAYMENT_ANSWER_REGEX = /Yes/
 
 	onMount(() => {
 		const url = $page.url
 		const params = new URLSearchParams(url.search)
-		const redirectToPayment = params.get('payment') === 'true'
+		const paymentAnswer = params.get('payment')
+		const redirectToPayment = paymentAnswer && PAYMENT_ANSWER_REGEX.test(paymentAnswer)
 
 		if (redirectToPayment) {
 			// Construct Stripe payment link with remaining parameters
