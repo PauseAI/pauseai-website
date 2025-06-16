4f2b3f09abbe0e60831931201e7a1c6e8bd5eee1
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-06-16 19:20:37 +0200

    Prerender /statement to avoid 500 responses

--- src/routes/statement/+page.ts
+++ src/routes/statement/+page.ts
@@ -1,17 +1,18 @@
-import type { Signatory } from '$lib/types';
+import type { Signatory } from '$lib/types'
 
-export const prerender = false;
+export const prerender = true // workaround for 500 responses
 
 export const load = async ({ fetch, setHeaders }) => {
-    const response = await fetch('api/signatories');
-    const { signatories, totalCount }: { signatories: Signatory[]; totalCount: number } = await response.json();
+	const response = await fetch('api/signatories')
+	const { signatories, totalCount }: { signatories: Signatory[]; totalCount: number } =
+		await response.json()
 
-    setHeaders({
-        'cache-control': 'public, max-age=3600' // 1 hour in seconds
-    });
+	setHeaders({
+		'cache-control': 'public, max-age=3600' // 1 hour in seconds
+	})
 
-    return {
-        signatories: signatories,
-        totalCount: totalCount
-    };
-};
+	return {
+		signatories: signatories,
+		totalCount: totalCount
+	}
+}
