2fbfc5872e9858a85128c994d24c55c8ea668021
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-07-01 12:01:24 +0200

    Hide US preemption banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -46,7 +46,7 @@
 
 <NearbyEvent contrast={hero} bind:eventFound bind:geo />
 {#if !eventFound}
-	{#if geo?.country?.code === 'US'}
+	{#if false && geo?.country?.code === 'US'}
 		<Banner>
 			<b
 				>ONE MORE REPUBLICAN NEEDED TO PROTECT STATE SOVEREIGNTY ON AI REGULATION | <ExternalLink
