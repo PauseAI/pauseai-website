21511eaa9fa84b227507c366372e8976c6652b77
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-08-06 23:55:44 +0200

    Use official Netlify types

--- src/routes/api/geo/+server.ts
+++ src/routes/api/geo/+server.ts
@@ -1,19 +1,15 @@
-import type { Platform } from '$lib/netlify'
-import { dev } from '$app/environment'
-
-export type GeoApiResponse = Platform['context']['geo']
+export type GeoApiResponse = App.Platform['context']['geo']
 
 export const prerender = false
 
 export function GET({ platform, setHeaders }) {
-	if (dev) {
-		console.warn('Skipping geo lookup in dev mode')
-		return new Response('Geo lookup is not available in development mode', {
+	if (!platform) {
+		console.warn('Skipping geo lookup, Platform not available in this environment')
+		return new Response('Geo lookup is not available in this environment', {
 			status: 501
 		})
 	}
-	const netlifyPlatform = platform as Readonly<Platform>
-	const geo: GeoApiResponse = netlifyPlatform.context.geo
+	const geo: GeoApiResponse = platform.context.geo
 	setHeaders({
 		'cache-control': 'private, max-age=3600' // local only, 1 hour in seconds
 	})
