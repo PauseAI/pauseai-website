5fd59c1aede84a970bc7aa7cec572a5bd2481586
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-08-29 15:19:21 +0200

    Register open letter meta

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -7,6 +7,7 @@ import { meta as quotesMeta } from '../../quotes/meta'
 import { meta as emailBuilderMeta } from '../../email-builder/meta'
 import { meta as teamsMeta } from '../../teams/meta'
 import { meta as statementMeta } from '../../statement/meta'
+import { meta as dearSirDemisMeta } from '../../dear-sir-demis-2025/meta'
 
 /** When adding an extra route, make sure to add the metadata here for SEO purposes */
 const hardCodedPages: Post[] = [
@@ -16,7 +17,8 @@ const hardCodedPages: Post[] = [
 	quotesMeta,
 	emailBuilderMeta,
 	teamsMeta,
-	statementMeta
+	statementMeta,
+	dearSirDemisMeta
 ]
 
 async function getPosts() {
