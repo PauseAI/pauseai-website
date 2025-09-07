60930e73c6f13841432956bc732d094e3ecf91cd
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-09-07 11:45:03 +0200

    #Paraglide plugin import error: @inlang/plugin-paraglide-js-adapter not found #445

--- project.inlang/default-settings.js
+++ project.inlang/default-settings.js
@@ -10,8 +10,7 @@ export default {
 	locales: ['en', 'de', 'nl'],
 	modules: [
 		'https://cdn.jsdelivr.net/npm/@inlang/plugin-m-function-matcher@latest/dist/index.js',
-		'https://cdn.jsdelivr.net/npm/@inlang/plugin-message-format@latest/dist/index.js',
-		'https://cdn.jsdelivr.net/npm/@inlang/plugin-paraglide-js-adapter@latest/dist/index.js'
+		'https://cdn.jsdelivr.net/npm/@inlang/plugin-message-format@latest/dist/index.js'
 	],
 	'plugin.inlang.messageFormat': {
 		pathPattern: './l10n-cage/json/{locale}.json'
