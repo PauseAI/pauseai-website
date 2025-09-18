e87d2d929451e340c1863eceb8e6cac9bcb161cc
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-09-18 17:55:51 +0200

    Replace deprecated ts.config

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,12 @@
 import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
 import svelte from 'eslint-plugin-svelte'
-import { globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import ts from 'typescript-eslint'
 import svelteConfig from './svelte.config.js'
 
-export default ts.config(
+export default defineConfig(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs.recommended,
