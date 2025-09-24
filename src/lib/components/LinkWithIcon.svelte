679768c28b1b8a4e703e30ac5c274b73421cf05d
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-09-24 13:48:22 +0200

    Divide up link component

---
+++ src/lib/components/LinkWithIcon.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Link, { Type } from './Link.svelte'
+	import ExternalLink from 'lucide-svelte/icons/external-link'
+	import Mail from 'lucide-svelte/icons/mail'
+
+	export let href: string
+	export let target: string | null = null
+	let className: string = ''
+	export { className as class }
+	export let hideIcon = false
+	export let rel: string | null = null
+
+	const ICON_PROPS = { size: '0.7em' }
+
+	// Link component determines the type
+	let type: Type
+</script>
+
+<Link {href} {target} {rel} class={className} bind:type>
+	<slot />{#if type != Type.Internal && !hideIcon}
+		<span style="white-space: nowrap">
+			<div class="icon">
+				{#if type == Type.External}
+					<ExternalLink {...ICON_PROPS} />
+				{:else if type == Type.Mail}
+					<Mail {...ICON_PROPS} />
+				{/if}
+			</div>
+		</span>
+	{/if}
+</Link>
+
+<style>
+	.icon {
+		display: inline-flex;
+		vertical-align: baseline;
+		margin-left: 0.1em;
+	}
+</style>
