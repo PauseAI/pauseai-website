f103870e44c8641eb3136e7417ce589645c798ca
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-11-02 11:09:36 +0100

    Responsive navbar skeleton

---
+++ src/lib/components/navbar/Navbar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import DesktopNavbar from './desktop/DesktopNavbar.svelte'
+	import MobileNavbar from './mobile/MobileNavbar.svelte'
+</script>
+
+<div class="desktop-navbar">
+	<DesktopNavbar>
+		<slot />
+	</DesktopNavbar>
+</div>
+<div class="mobile-navbar">
+	<MobileNavbar>
+		<slot />
+	</MobileNavbar>
+</div>
+
+<style>
+	.desktop-navbar {
+		display: default;
+	}
+
+	.mobile-navbar {
+		display: none;
+	}
+
+	@media (max-width: 600px) {
+		.desktop-navbar {
+			display: none;
+		}
+
+		.mobile-navbar {
+			display: default;
+		}
+	}
+</style>
---
+++ src/lib/components/navbar/Navlink.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import DesktopNavlink from './desktop/DesktopNavbar.svelte'
+	import MobileNavlink from './mobile/MobileNavbar.svelte'
+</script>
+
+<span class="desktop-navlink">
+	<DesktopNavlink>
+		<slot />
+	</DesktopNavlink>
+</span>
+<span class="mobile-navlink">
+	<MobileNavlink>
+		<slot />
+	</MobileNavlink>
+</span>
+
+<style>
+	:global(.mobile-navbar) .desktop-navlink {
+		display: none;
+	}
+
+	:global(.desktop-navbar) .mobile-navlink {
+		display: none;
+	}
+</style>
