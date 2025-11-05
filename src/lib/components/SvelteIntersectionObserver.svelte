6f6887634b09aca517845e1787074868398ea068
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-11-05 14:28:17 +0100

    Animate stats on homepage (#534)
    * Reapply "Animate stats on homepage"
    
    * Avoid reserved name IntersectionObserver
    
    * Don't try to disconnect IntersectionObserver on the server

---
+++ src/lib/components/SvelteIntersectionObserver.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	export let isIntersecting: boolean = false
+	export let rootMargin: string = '0px'
+	export let threshold: number | number[] = 0
+
+	let element: HTMLElement
+	let observer: IntersectionObserver
+
+	onMount(() => {
+		observer = new IntersectionObserver(
+			(entries) => {
+				isIntersecting = entries[0].isIntersecting
+			},
+			{ rootMargin, threshold }
+		)
+		observer.observe(element)
+		return () => observer.disconnect()
+	})
+</script>
+
+<div bind:this={element}>
+	<slot />
+</div>
