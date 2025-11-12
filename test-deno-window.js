63584620584436b0cafae2691610d874361d8492
Anthony Bailey <mail@anthonybailey.net>
2025-11-12 19:16:02 +0000

    Remove test-deno-window.js file causing lint error
    File was used for local testing but triggers ESLint 'Deno is not defined' error in CI

--- test-deno-window.js
+++
@@ -1,8 +0,0 @@
-console.log('Deno version:', Deno.version.deno)
-console.log('typeof window:', typeof window)
-if (typeof window !== 'undefined') {
-	console.log('typeof window.location:', typeof window.location)
-	if (typeof window.location !== 'undefined') {
-		console.log('window.location:', window.location)
-	}
-}
