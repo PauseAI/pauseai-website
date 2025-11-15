d29c11d93fca1f61e2ab5a563521e8e2f134db63
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-11-15 16:28:55 +0100

    Revert dummy edit

--- src/routes/people/meta.ts
+++ src/routes/people/meta.ts
@@ -1,7 +1,7 @@
 import type { Post } from '$lib/types'
 
 export const meta: Post = {
-	title: 'People of PauseAI', // Whitespace change to trigger rebuild
+	title: 'People of PauseAI',
 	description: 'Staff and contributors behind PauseAI',
 	date: '2025-11-06',
 	slug: 'people',
