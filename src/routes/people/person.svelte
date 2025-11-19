bc1fe00906202f327eee8fc6fc4e03eebc201105
Patricio <patriciovercesi@hotmail.com>
2025-11-19 13:54:05 -0300

    deleting bio traces 2

--- src/routes/people/person.svelte
+++ src/routes/people/person.svelte
@@ -16,7 +16,7 @@
 		</div>
 
 		{#if title}
-			<div class="bio">
+			<div class="title">
 				{title}
 			</div>
 		{/if}
@@ -24,45 +24,45 @@
 </li>
 
 <style>
-    .person {
-        display: flex;
-        gap: 1rem;
-    }
-    .details {
-        flex-shrink: 1;
-    }
-    .image {
-        display: flex;
-        flex-basis: 100px;
-        height: 100px;
-        align-items: center;
-        flex-shrink: 0;
-        justify-content: center;
-        border-radius: 50%;
-        background-size: cover;
-        background-position: center;
-    }
-    
-    .name-title {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        gap: 0.1rem; /* Reduced gap */
-    }
-    .name {
-        color: var(--text);
-        font-family: var(--font-heading);
-        font-weight: bold;
-        text-decoration: none;
-        font-size: 1.4rem;
-        text-transform: capitalize;
-        margin: 0;
-    }
+	.person {
+		display: flex;
+		gap: 1rem;
+	}
+	.details {
+		flex-shrink: 1;
+	}
+	.image {
+		display: flex;
+		flex-basis: 100px;
+		height: 100px;
+		align-items: center;
+		flex-shrink: 0;
+		justify-content: center;
+		border-radius: 50%;
+		background-size: cover;
+		background-position: center;
+	}
 
-    .bio {
-        font-weight: normal;
-        font-family: var(--font-body);
-        font-size: 1rem;
-        margin-top: 0.2rem;
+	.name-title {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 0.1rem; /* Reduced gap */
+	}
+	.name {
+		color: var(--text);
+		font-family: var(--font-heading);
+		font-weight: bold;
+		text-decoration: none;
+		font-size: 1.4rem;
+		text-transform: capitalize;
+		margin: 0;
+	}
 
+	.title {
+		font-weight: normal;
+		font-family: var(--font-body);
+		font-size: 1rem;
+		margin-top: 0.2rem;
+	}
 </style>
