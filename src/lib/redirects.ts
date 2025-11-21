4300cda775ec39cb25d7d680931df9d67c0c4844
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-11-21 13:47:58 +0100

    Redirect /people to /about

--- src/lib/redirects.ts
+++ src/lib/redirects.ts
@@ -3,7 +3,8 @@ import { redirect } from '@sveltejs/kit'
 const REDIRECTS: Record<string, string> = {
 	'/2025-feb': '/2025-february',
 	'/collages/manual_bootstrap.jpg': '/api/images/collages/manual_bootstrap.jpg',
-	'/selfie': '/sayno'
+	'/selfie': '/sayno',
+	'/people': '/about'
 }
 
 export function handleRedirects(path: string) {
