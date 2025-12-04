84b7e68c6b80a14a553104cf377d206ae3eeb146
Wituareard <87208350+Wituareard@users.noreply.github.com>
2025-12-04 11:53:47 +0100

    Didn't commit all files for some reason

---
+++ src/lib/components/IconBullet.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { Icon } from 'lucide-svelte'
+	import MessageWithLink from '$lib/components/MessageWithLink.svelte'
+
+	export let icon: typeof Icon
+	export let content: () => string
+</script>
+
+<div class="icon-bullet" role="listitem">
+	<span class="icon">
+		<svelte:component this={icon} size="1em" />
+	</span>
+	<span>
+		<MessageWithLink content={content()} />
+	</span>
+</div>
+
+<style>
+	.icon-bullet {
+		display: flex;
+		gap: 0.5em;
+		margin-bottom: 0.4em;
+	}
+
+	.icon {
+		margin-top: 0.2em;
+	}
+</style>
