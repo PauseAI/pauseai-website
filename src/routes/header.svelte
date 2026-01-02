6b3e9a7705ebee6b9389a76dd392e4ad94802d87
Patricio <patriciovercesi@hotmail.com>
2026-01-02 17:40:10 -0300

    changed headers

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -13,17 +13,12 @@
 </script>
 
 <Navbar {inverted} {moveUp}>
-	<Navlink {inverted} first href="/learn">{m.header_learn()}</Navlink>
+	<Navlink {inverted} first href="/join">{m.header_join()}</Navlink>
+	<Navlink {inverted} href="/about">About Us</Navlink>
+	<Navlink {inverted} href="/learn">{m.header_learn()}</Navlink>
 	<Navlink {inverted} href="/proposal">{m.header_proposal()}</Navlink>
-	<Navlink {inverted} href="/communities">GROUPS</Navlink>
-	<Navlink {inverted} href="/faq">{m.header_faq()}</Navlink>
-	<Navlink {inverted} href="/action">{m.header_action()}</Navlink>
-	<Navlink {inverted} href="/donate">{m.header_donate()}</Navlink>
-	{#if enableBot}
-		<Navlink {inverted} href="/chat">{botName}</Navlink>
-	{/if}
-	<!-- <NavLink href="/about">About</NavLink> -->
-	<Navlink {inverted} c2a href="/join">{m.header_join()}</Navlink>
+	<Navlink {inverted} href="/action">Act</Navlink>
+	<Navlink {inverted} href="/communities">Groups</Navlink>
 	<LanguageSwitcher {inverted} />
 	<Navlink {inverted} href="/search" ariaLabel="Search"><SearchIcon size="0.8em" /></Navlink>
 </Navbar>
