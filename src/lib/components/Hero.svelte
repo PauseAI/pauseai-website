63f48306749d16c93ae1c0df45d3f1cf58f0c562
RisingOrange <jakub.fidler@protonmail.com>
2026-02-23 12:39:22 +0100

    Remove redundant picture CSS rule

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -61,12 +61,6 @@
 		text-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
 	}
 
-	.hero :global(picture) {
-		display: block;
-		width: 100%;
-		height: 100%;
-	}
-
 	.hero :global(img) {
 		width: 100%;
 		height: 100%;
