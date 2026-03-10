4b89235f68422c008c2fed9c1451b47f817e4b43
Wituareard <87208350+Wituareard@users.noreply.github.com>
2026-03-10 21:07:50 +0100

    Optimize press logos

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -3,6 +3,7 @@
 
 	export let src: string
 	export let alt: string | null = null
+	export let title: string | null = null
 	export let sizes: string | null = null
 	let className: string = ''
 	export { className as class }
@@ -18,22 +19,32 @@
 			}
 		}
 	)
+	const assetUrlModules = import.meta.glob<string>('../../assets/images/**/*', {
+		eager: true,
+		import: 'default',
+		query: { url: true }
+	})
 
 	let picture: Picture | null = null
+	let assetUrl: string | null = null
 
 	if (src.startsWith('/')) {
 		const fullPath = `../../assets/images${src}`
 		if (pictureModules[fullPath]) {
 			picture = pictureModules[fullPath]
+		} else if (assetUrlModules[fullPath]) {
+			assetUrl = assetUrlModules[fullPath]
 		}
 	}
 </script>
 
 {#if picture}
-	<enhanced:img src={picture} {alt} class="enhanced {className}" loading="lazy" {sizes}
+	<enhanced:img src={picture} {alt} class="enhanced {className}" loading="lazy" {sizes} {title}
 	></enhanced:img>
+{:else if assetUrl}
+	<img src={assetUrl} {alt} {title} loading="lazy" class={className} />
 {:else}
-	<img {src} {alt} loading="lazy" class={className} />
+	<img {src} {alt} {title} loading="lazy" class={className} />
 {/if}
 
 <style>
