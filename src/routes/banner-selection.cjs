402b177d6a81daf19c3b8ca99d7fd0315e118342
Wituareard <87208350+Wituareard@users.noreply.github.com>
2026-03-25 15:37:03 +0100

    Put banners in array

--- src/routes/banner-selection.cjs
+++ src/routes/banner-selection.cjs
@@ -29,34 +29,61 @@ window.selectBanners = function () {
 		return true
 	}
 
+	var mainBannerRules = [
+		{
+			id: 'gb-feb28-protest',
+			countries: ['GB'],
+			dateRange: [null, '2025-02-28']
+		},
+		{
+			id: 'us-state-sovereignty',
+			countries: ['US'],
+			dateRange: [null, '2025-02-28']
+		},
+		{
+			id: 'holiday-littlehelpers',
+			countries: null,
+			dateRange: [null, '2024-12-31']
+		}
+	]
+
+	var campaignBannerRules = [
+		{
+			id: 'brussels-ep-protest-2026',
+			dateRange: [null, '2026-02-23']
+		}
+	]
+
+	function findActiveBannerRule(rules, dismissalPrefix) {
+		return rules.find(function (rule) {
+			var countryMatches = !rule.countries || rule.countries.indexOf(country) !== -1
+			if (
+				countryMatches &&
+				inDateRange(...rule.dateRange) &&
+				!dismissed(dismissalPrefix, rule.id)
+			) {
+				return true
+			}
+			return false
+		})
+	}
+
 	// Main banner: first matching rule wins
 	delete document.documentElement.dataset.activeBanner
 	delete document.documentElement.dataset.isActiveBannerGeo
-	if (
-		country === 'GB' &&
-		inDateRange(null, '2025-02-28') &&
-		!dismissed('banner', 'gb-feb28-protest')
-	) {
-		document.documentElement.dataset.activeBanner = 'gb-feb28-protest'
-		document.documentElement.dataset.isActiveBannerGeo = 'true'
-	} else if (
-		country === 'US' &&
-		inDateRange(null, '2025-02-28') &&
-		!dismissed('banner', 'us-state-sovereignty')
-	) {
-		document.documentElement.dataset.activeBanner = 'us-state-sovereignty'
-		document.documentElement.dataset.isActiveBannerGeo = 'true'
-	} else if (inDateRange(null, '2024-12-31') && !dismissed('banner', 'holiday-littlehelpers')) {
-		document.documentElement.dataset.activeBanner = 'holiday-littlehelpers'
+	var activeMainBannerRule = findActiveBannerRule(mainBannerRules, 'banner')
+	if (activeMainBannerRule) {
+		document.documentElement.dataset.activeBanner = activeMainBannerRule.id
+		if (activeMainBannerRule.countries) {
+			document.documentElement.dataset.isActiveBannerGeo = 'true'
+		}
 	}
 
 	// Campaign banner: shown if eligible
 	delete document.documentElement.dataset.activeCampaignBanner
-	if (
-		inDateRange(null, '2026-02-23') &&
-		!dismissed('campaign_banner', 'brussels-ep-protest-2026')
-	) {
-		document.documentElement.dataset.activeCampaignBanner = 'brussels-ep-protest-2026'
+	var activeCampaignBannerRule = findActiveBannerRule(campaignBannerRules, 'campaign_banner')
+	if (activeCampaignBannerRule) {
+		document.documentElement.dataset.activeCampaignBanner = activeCampaignBannerRule.id
 	}
 }
 window.selectBanners()
