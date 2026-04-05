c81a48099a58aa1e436a766e559f9fff697beeef
Wituareard <87208350+Wituareard@users.noreply.github.com>
2026-04-05 19:45:03 +0200

    Fix ESLint comment

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-restricted-syntax
+// eslint-disable-next-line no-restricted-syntax
 export const SENTRY_RELEASE = import.meta.env.SENTRY_RELEASE as string | undefined
