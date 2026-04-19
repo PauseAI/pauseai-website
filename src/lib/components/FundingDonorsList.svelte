923eeeb6a77a24c805128a0c580ef6c70e932f90
Wituareard <87208350+Wituareard@users.noreply.github.com>
2026-04-20 01:45:10 +0200

    Add skeleton loading with deterministic random widths to donor list

--- src/lib/components/FundingDonorsList.svelte
+++ src/lib/components/FundingDonorsList.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import Link from '$lib/components/Link.svelte'
 	import { getTopPublicDonors } from '$lib/funding-donors.remote'
+	import Skeleton from '$lib/components/Skeleton.svelte'
 
 	const donorsPromise = getTopPublicDonors()
 </script>
 
-{#await donorsPromise then donors}
+{#await donorsPromise}
+	<ul>
+		{#each Array.from({ length: 15 }) as _, i}
+			<li>
+				<Skeleton width="50px" />
+				<Skeleton width="200px" random seed={i} />
+			</li>
+		{/each}
+	</ul>
+{:then donors}
 	<ul>
 		{#each donors as d}
 			<li>
