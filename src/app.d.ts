8e820d9b2d4b3ff63f5428cd7a34213b59bdac24
Wituareard <87208350+Wituareard@users.noreply.github.com>
2026-05-14 15:21:55 +0200

    Override "any" return type of Response#json

--- src/app.d.ts
+++ src/app.d.ts
@@ -14,6 +14,10 @@ declare global {
 		}
 	}
 
+	interface Response {
+		json(): Promise<unknown> // Override "any"
+	}
+
 	interface Twttr {
 		ready: (callback: () => void) => void
 		load: (element: HTMLElement) => void
