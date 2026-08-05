2a8b220c25cdaaffc59b11cdfb92845459f80b27
Wituareard <87208350+Wituareard@users.noreply.github.com>
2026-08-05 23:25:23 +0200

    Extract /about loader component and convert route to markdown post

--- src/routes/about/meta.ts
+++
@@ -1,9 +0,0 @@
-import type { StrictPost } from '$lib/types'
-
-export const meta: StrictPost = {
-	title: 'About Us',
-	description: 'Staff behind PauseAI',
-	date: '2025-11-06',
-	slug: 'about',
-	categories: []
-}
